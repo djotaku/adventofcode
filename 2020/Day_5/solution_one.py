8a5552141589518758ff0c5005035779945ac93c
Eric Mesa <ericsbinaryworld@gmail.com>
2020-12-05 13:10:26 -0500

    Solution 1 completed
    First time using pytest during Advent of Code

--- 2020/Day_5/solution_one.py
+++ 2020/Day_5/solution_one.py
@@ -1,13 +1,34 @@
-def find_row_column(seat_directions):
-    lower = 0
-    upper = 127
-    letters = seat_directions[0:7]
-    for letter in letters:
-        if letter == "F":
+def do_the_loop(ticket_letters, lower_letter, lower_bound, upper_bound):
+    lower = lower_bound
+    upper = upper_bound
+    for letter in ticket_letters:
+        if letter == lower_letter:
             upper = (upper + lower) // 2
         else:
             lower = (upper + lower) // 2 + 1
-    return lower, upper
+    if ticket_letters[-1] == lower_letter:
+        return lower
+    else:
+        return upper
+
+
+def find_row_column(seat_directions):
+    row_letters = seat_directions[0:7]
+    column_letters = seat_directions[7:]
+    return do_the_loop(row_letters, "F", 0, 127), do_the_loop(column_letters, "L", 0, 7)
+
+
+def find_seat_id(row_column):
+    return (row_column[0] * 8) + row_column[1]
+
+
+def find_highest_seat_id(seats):
+    sorted_seats = sorted(seats)
+    return sorted_seats[-1]
+
 
 if __name__ == "__main__":
-    print(find_row_column("FBFBBFFRLR"))
+    with open("input", 'r') as file:
+        tickets = file.readlines()
+        seat_ids = [find_seat_id(find_row_column(ticket)) for ticket in tickets]
+        print(find_highest_seat_id(seat_ids))
--- 2020/Day_5/test_solution_1.py
+++ 2020/Day_5/test_solution_1.py
@@ -1,6 +1,20 @@
 from . import solution_one
 
 
-def test_one():
-    row_column = solution_one.find_row_column("FBFBBFFRLR")
-    assert row_column == (44, 5)
+def test_row_column():
+    assert solution_one.find_row_column("FBFBBFFRLR") == (44, 5)
+    assert solution_one.find_row_column("BFFFBBFRRR") == (70, 7)
+    assert solution_one.find_row_column("FFFBBBFRRR") == (14, 7)
+    assert solution_one.find_row_column("BBFFBBFRLL") == (102, 4)
+
+
+def test_seat_id():
+    assert solution_one.find_seat_id((44, 5)) == 357
+    assert solution_one.find_seat_id((70, 7)) == 567
+    assert solution_one.find_seat_id((14, 7)) == 119
+    assert solution_one.find_seat_id((102, 4)) == 820
+
+
+def test_highest_seat():
+    assert solution_one.find_highest_seat_id([2, 99, 45, 989, 34, 0]) == 989
+
