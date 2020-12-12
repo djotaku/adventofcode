06ef10162f192c9895c9fd1bd7527b6dc2d84b1d
Eric Mesa <ericsbinaryworld@gmail.com>
2020-12-12 11:53:33 -0500

    passes ref input, but apparently hitting a corner case

--- 2020/Day_12/solution_1.py
+++ 2020/Day_12/solution_1.py
@@ -1,3 +1,57 @@
 def parse_input(file_input):
     with open(file_input, 'r') as file:
         return [(row[0], int(row[1:].rstrip())) for row in file.readlines()]
+
+
+def orientation(directions, current_orientation):
+    cardinal_directions = ['north', 'east', 'south', 'west']
+    position_in_directions = cardinal_directions.index(current_orientation)
+    degrees = directions[1]
+    while degrees !=0:
+        if directions[0] == 'R':
+            position_in_directions += 1
+            if position_in_directions == len(cardinal_directions):
+                position_in_directions = 0
+            degrees -= 90
+        elif directions[0] == 'L':
+            position_in_directions -= 1
+            if position_in_directions < 0:
+                position_in_directions = len(cardinal_directions) - 1
+            degrees -= 90
+    return cardinal_directions[position_in_directions]
+
+
+def move_boat(directions):
+    north_south = 0
+    east_west = 0
+    boat_orientation = 'east'
+    for direction in directions:
+        print(f'{north_south}')
+        print(f'{east_west}')
+        print('------------------')
+        if direction[0] == 'F':
+            if boat_orientation == 'east':
+                east_west += direction[1]
+            elif boat_orientation == 'west':
+                east_west -= direction[1]
+            elif boat_orientation == 'north':
+                north_south += direction[1]
+            elif boat_orientation == 'south':
+                north_south -= direction[1]
+        elif direction[0] == 'L' or direction[0] == 'R':
+            boat_orientation = orientation(direction, boat_orientation)
+        elif direction[0] == 'N':
+            north_south += direction[1]
+        elif direction[0] == 'S':
+            north_south -= direction[1]
+        elif direction[0] == 'E':
+            north_south += direction[1]
+        elif direction[0] == 'W':
+            north_south -= direction[1]
+    return abs(north_south) + abs(east_west)
+
+
+if __name__ == "__main__":
+    print(move_boat(parse_input('input')))
+
+# 1593 is too high
--- 2020/Day_12/test_solution_1.py
+++ 2020/Day_12/test_solution_1.py
@@ -3,3 +3,18 @@ from . import solution_1
 
 def test_parse_input():
     assert solution_1.parse_input('ref_input') == [("F", 10), ("N", 3), ("F", 7), ("R", 90), ("F", 11)]
+
+
+def test_orientation():
+    assert solution_1.orientation(('R', 90), 'north') == 'east'
+    assert solution_1.orientation(('L', 90), 'north') == 'west'
+    assert solution_1.orientation(('R', 180), 'north') == "south"
+    assert solution_1.orientation(('R', 270), 'north') == "west"
+    assert solution_1.orientation(('R', 360), 'north') == "north"
+    assert solution_1.orientation(('L', 180), 'north') == 'south'
+    assert solution_1.orientation(('L', 270), 'north') == 'east'
+
+
+def test_move_boat():
+    directions = solution_1.parse_input('ref_input')
+    assert solution_1.move_boat(directions) == 25
