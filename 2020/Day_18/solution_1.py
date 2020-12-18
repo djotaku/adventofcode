8a75dc07cf3957e640134ea4b61a01b96bb36e66
Eric Mesa <ericsbinaryworld@gmail.com>
2020-12-18 15:45:13 -0500

    the math works for simple parens now!!

--- 2020/Day_18/solution_1.py
+++ 2020/Day_18/solution_1.py
@@ -6,7 +6,6 @@ def evaluate_parenthesis(equation):
         equation_with_parenthesis = []
         parenthesis_left_index = []
         for index in range(0, len(equation)):
-            print(equation[index])
             if equation[index] == "(":
                 parenthesis_count += 1
                 parenthesis_left_index.append(index)
@@ -20,7 +19,6 @@ def evaluate_parenthesis(equation):
         return equation_with_parenthesis
 
 
-
 def parse_input(input_file):
     with open(input_file, 'r') as file:
         equations = [[char for char in line if char != ' '] for line in file.readlines()]
@@ -32,12 +30,12 @@ def new_math(math_stack):
     final_sum = 0
     left_number = 0
     operator = ''
-    parenthesis = False
-    parenthesis_count = 0
-    parenthesis_list = []
-    print(f'{parenthesis}')
     for item in math_stack:
-        if item.isdigit() and not parenthesis:
+        if isinstance(item, list):
+            paren_sum = new_math(item)
+            final_sum = eval(f'{left_number}{operator}{paren_sum}')
+            left_number = final_sum
+        elif item.isdigit():
             if left_number == 0:
                 left_number = item
             else:
@@ -45,28 +43,6 @@ def new_math(math_stack):
                 print(f'{left_number}{operator}{item}')
                 final_sum = eval(f'{left_number}{operator}{item}')
                 left_number = final_sum
-        elif parenthesis:
-            if item != ')' and parenthesis_count >= 1:
-                if item == '(':
-                    parenthesis_count += 1
-                parenthesis_list.append(item)
-                print(f'{parenthesis_list=}')
-            elif item == ')' and parenthesis_count >= 1:
-                print("end of parenthesis")
-                paren_sum = new_math(parenthesis_list.copy())
-                print(f'{paren_sum=}')
-                print(f'{left_number=}')
-                print(f'{operator=}')
-                parenthesis_count -= 1
-                parenthesis_list.clear()
-                parenthesis = False
-                final_sum = eval(f'{left_number}{operator}{paren_sum}')
-                left_number = final_sum
-        elif item == "(":
-            parenthesis = True
-            parenthesis_count += 1
-        #elif item == ')':
-        #    parenthesis = False
         else:
             operator = item
         #print('--------')
--- 2020/Day_18/test_solution_1.py
+++ 2020/Day_18/test_solution_1.py
@@ -13,11 +13,13 @@ def test_parse_input_complex_parenthesis():
     assert solution_1.parse_input('ref_input_2') == [['1', '+', ['2', '*', '3'], '+', ['4', '*', ['5', '+', '6']]]]
 
 
-#def test_new_math_no_parens():
-#    assert solution_1.new_math(['1', "+", '2', '*', '3', '+', '4', '*', '5', '+', '6']) == 71
+def test_new_math_no_parens():
+    assert solution_1.new_math(['1', "+", '2', '*', '3', '+', '4', '*', '5', '+', '6']) == 71
 
 
-#def test_new_math_parens():
-#    equation = solution_1.parse_input('ref_input_2')
-#    print(equation[0])
-#    assert solution_1.new_math(equation[0]) == 51
+def test_new_math_simple_parens():
+    equation = solution_1.parse_input('ref_input_3')
+    print(equation[0])
+    assert solution_1.new_math(equation[0]) == 26
+
+
