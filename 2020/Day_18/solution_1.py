f5687527394de5cd539796b5107a764646c48075
Eric Mesa <ericsbinaryworld@gmail.com>
2020-12-18 15:35:21 -0500

    ok, I've changed my parsing to make sublists for the parens

--- 2020/Day_18/solution_1.py
+++ 2020/Day_18/solution_1.py
@@ -1,6 +1,30 @@
+def evaluate_parenthesis(equation):
+    if '(' not in equation:
+        return equation
+    else:
+        parenthesis_count = 0
+        equation_with_parenthesis = []
+        parenthesis_left_index = []
+        for index in range(0, len(equation)):
+            print(equation[index])
+            if equation[index] == "(":
+                parenthesis_count += 1
+                parenthesis_left_index.append(index)
+            if equation[index] == ")":
+                parenthesis_count -= 1
+                if parenthesis_count == 0:
+                    equation_with_parenthesis.append(evaluate_parenthesis(equation[parenthesis_left_index[0]+1:index]))
+            if parenthesis_count == 0 and equation[index] != '(' and equation[index] != ")":
+                equation_with_parenthesis.append(equation[index])
+        return equation_with_parenthesis
+
+
+
 def parse_input(input_file):
     with open(input_file, 'r') as file:
-        return [[char for char in line if char != ' '] for line in file.readlines()]
+        equations = [[char for char in line if char != ' '] for line in file.readlines()]
+        fixed_equations = [evaluate_parenthesis(equation) for equation in equations]
+        return fixed_equations
 
 
 def new_math(math_stack):
@@ -10,6 +34,7 @@ def new_math(math_stack):
     parenthesis = False
     parenthesis_count = 0
     parenthesis_list = []
+    print(f'{parenthesis}')
     for item in math_stack:
         if item.isdigit() and not parenthesis:
             if left_number == 0:
--- 2020/Day_18/test_solution_1.py
+++ 2020/Day_18/test_solution_1.py
@@ -5,11 +5,15 @@ def test_parse_input():
     assert solution_1.parse_input('ref_input') == [['1', "+", '2', '*', '3', '+', '4', '*', '5', '+', '6']]
 
 
-def test_new_math_no_parens():
-    assert solution_1.new_math(['1', "+", '2', '*', '3', '+', '4', '*', '5', '+', '6']) == 71
+def test_parse_input_simple_parenthesis():
+    assert solution_1.parse_input('ref_input_3') == [['2', '*', '3', '+', ['4', '*', '5']]]
 
 
-def test_new_math_parens():
-    equation = solution_1.parse_input('ref_input_2')
-    print(equation[0])
-    assert solution_1.new_math(equation[0]) == 51
+#def test_new_math_no_parens():
+#    assert solution_1.new_math(['1', "+", '2', '*', '3', '+', '4', '*', '5', '+', '6']) == 71
+
+
+#def test_new_math_parens():
+#    equation = solution_1.parse_input('ref_input_2')
+#    print(equation[0])
+#    assert solution_1.new_math(equation[0]) == 51
