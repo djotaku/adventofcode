3766d890759ced04354487735028cda89644f074
Eric Mesa <ericsbinaryworld@gmail.com>
2021-04-11 12:46:32 -0400

    Perl part 1 completed

--- 2015/Day_04/Perl/part_1.pl
+++ 2015/Day_04/Perl/part_1.pl
@@ -6,3 +6,14 @@ use Digest::MD5 qw(md5_hex);
 
 my $puzzle_input = "iwrupvqb";
 
+my $number = 0;
+
+my $hex_test = md5_hex("$puzzle_input$number");
+
+until ($hex_test =~ /^00000/){
+
+	$number++;
+	$hex_test = md5_hex("$puzzle_input$number");
+}
+
+say "Santa's magic number is $number";
