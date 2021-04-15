45fa50657ef88152467f96f0284f7719137f52dc
Eric Mesa <ericsbinaryworld@gmail.com>
2021-04-15 18:00:34 -0400

    ruby solution complete

--- 2015/Day_05/Ruby/part_1.rb
+++ 2015/Day_05/Ruby/part_1.rb
@@ -23,3 +23,14 @@ end
 def naughty_or_nice(line_to_evaluate)
     !! rule_one(line_to_evaluate) and rule_two(line_to_evaluate) and rule_three(line_to_evaluate)
 end
+
+if $PROGRAM_NAME == __FILE__
+    naughty_or_nice_list = input_per_line('../input.txt')
+    nice_count = 0
+    naughty_or_nice_list.each do |line|
+        if naughty_or_nice(line)
+            nice_count += 1
+        end
+    end
+    puts "Santa has #{nice_count} nice children."
+end
