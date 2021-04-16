f553756fb3c64beb1dbb224679e27780c5c319a8
Eric Mesa <ericsbinaryworld@gmail.com>
2021-04-15 21:11:05 -0400

    trying to debug Perl

--- 2015/Day_05/Perl/part_1.pl
+++ 2015/Day_05/Perl/part_1.pl
@@ -12,7 +12,8 @@ my $nice_count = 0;
 for my $line (@naughty_or_nice_list){
     if (my @matches = $line =~ m/[aeiou]/g >= 3 && $line =~ /(.)\1/ && $line =~ !/ab|cd|pq|xy/)
     {
-	    $nice_count++;
+	    say scalar @matches; # this outputs 1.....
+    	$nice_count++;
     }
 }
 
