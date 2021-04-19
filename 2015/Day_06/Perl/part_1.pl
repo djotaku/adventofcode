2bcbea357df56566bd22d824b6de83ef823e887c
Eric Mesa <ericsbinaryworld@gmail.com>
2021-04-19 19:21:37 -0400

    regex works

--- 2015/Day_06/Perl/part_1.pl
+++ 2015/Day_06/Perl/part_1.pl
@@ -11,8 +11,8 @@ my %lights;
 
 for my $instruction (@lighting_instructions){
 
-    my @regex = $instruction =~ /([a-z]* [a-z]*|[a-z]*) (\d*),(\d*)/g;
+    my ($verb, $starting_x, $starting_y, $through, $end_x, $end_y) = $instruction =~ /([a-z]* [a-z]*|[a-z]*) (\d*),(\d*)/g;
     
-    say @regex
+    say $verb;
 
 }
