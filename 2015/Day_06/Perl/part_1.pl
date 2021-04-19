8d1672855b713cd1e11e8d529032dcb66df1dbfd
Eric Mesa <ericsbinaryworld@gmail.com>
2021-04-19 19:22:30 -0400

    broked up

--- 2015/Day_06/Perl/part_1.pl
+++ 2015/Day_06/Perl/part_1.pl
@@ -13,6 +13,6 @@ for my $instruction (@lighting_instructions){
 
     my ($verb, $starting_x, $starting_y, $through, $end_x, $end_y) = $instruction =~ /([a-z]* [a-z]*|[a-z]*) (\d*),(\d*)/g;
     
-    say $verb;
+    
 
 }
