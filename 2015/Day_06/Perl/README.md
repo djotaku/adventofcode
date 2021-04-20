bdead22614c6923242a1776f6e713d7c60ef2005
Eric Mesa <ericsbinaryworld@gmail.com>
2021-04-19 20:09:49 -0400

    My Part 1 Perl answer - off by 1

--- 2015/Day_06/Perl/README.md
+++ 2015/Day_06/Perl/README.md
@@ -1,6 +1,7 @@
 # What I learned on Day 06 from Perl
 
 ## Part 1
-
+- how to do ranges in Perl. Although I had to abandon it for a C-style for-loop to get my coordinates.
+- using hashes. 
 
 ## Part 2
--- 2015/Day_06/Perl/part_1.pl
+++ 2015/Day_06/Perl/part_1.pl
@@ -13,6 +13,47 @@ for my $instruction (@lighting_instructions){
 
     my ($verb, $starting_x, $starting_y, $through, $end_x, $end_y) = $instruction =~ /([a-z]* [a-z]*|[a-z]*) (\d*),(\d*)/g;
     
+    my @coordinates = [];
     
-
+    for (my $x = $starting_x; $x < $end_x + 1; $x++)
+    {
+        for (my $y= $starting_y; $y < $end_y + 1; $y++)
+        {
+            push(@coordinates, "($x,$y)");
+        }
+    }
+    for my $coordinate (@coordinates)
+    {
+        if ($verb eq "turn on")
+        {
+            $lights{$coordinate} = 1;
+        }
+        elsif ($verb eq "turn off")
+        {
+            $lights{$coordinate} = 0;
+        }
+        elsif ($verb eq "toggle")
+        {
+            if (exists $lights{$coordinate})
+            {
+                $lights{$coordinate} ^= 1;
+            }
+            else
+            {
+                $lights{$coordinate} = 1;
+            }
+        }
+        else
+        {
+            say "I should never get here.";
+        }
+    }
 }
+
+my $active_lights = 0;
+for my $value (values %lights)
+{
+    $active_lights += $value;
+}
+
+say "Santa has asked me to turn on $active_lights lights."
