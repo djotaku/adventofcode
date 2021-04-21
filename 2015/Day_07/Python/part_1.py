e07020009beec542f0ccae641bfbe11ccacf65b5
Eric Mesa <ericsbinaryworld@gmail.com>
2021-04-20 20:27:15 -0400

    Flabbergasted that this is working on my first try with the unit tests
    I just thought about it while swimming, then a bit a work. I sucked at recursion last Dec.

--- 2015/Day_07/Python/part_1.py
+++ 2015/Day_07/Python/part_1.py
@@ -15,7 +15,35 @@ def create_dictionary(instructions):
     return wires
 
 
+def break_up_equation(equation):
+    """Figure out the operand(s) and operation and return them."""
+    two_operand_regex = re.compile(r'([a-z]+) ([A-Z]*) (\w)')
+    one_operand_regex = re.compile('([A-Z]*) ([a-z]+)')
+    if re.match(two_operand_regex, equation):
+        result = re.findall(two_operand_regex, equation)
+        return result[0][0], result[0][1], result[0][2]
+    else:
+        result = re.findall(one_operand_regex, equation)
+        return result[0][0], result[0][1]
+
+
 def find_value_on_line(all_wires, wire_to_find):
     """Figure out what the final value is on a wire."""
     if all_wires[wire_to_find].isnumeric():
         return int(all_wires[wire_to_find])
+    else:
+        equation = break_up_equation(all_wires[wire_to_find])
+        if len(equation) == 3:
+            operand_left = find_value_on_line(all_wires, equation[0])
+            operand_right = find_value_on_line(all_wires, equation[2])
+            operation = equation[1]
+            if operation == "AND":
+                return operand_left & operand_right
+            elif operation == "LSHIFT":
+                return operand_left << operand_right
+            elif operation == "OR":
+                return operand_left | operand_right
+            elif operation == "RSHIFT":
+                return operand_left >> operand_right
+        else:
+            return ~ find_value_on_line(all_wires, equation[1])
--- 2015/Day_07/Python/test_part_1.py
+++ 2015/Day_07/Python/test_part_1.py
@@ -10,8 +10,20 @@ def test_create_dictionary():
     assert wire_dictionary['d'] == "x AND y"
 
 
+def test_break_up_equation():
+    equation = "x AND y"
+    assert part_1.break_up_equation(equation) == ('x', 'AND', 'y')
+    equation = "x LSHIFT 2"
+    assert part_1.break_up_equation(equation) == ('x', "LSHIFT", '2')
+    equation = "NOT x"
+    assert part_1.break_up_equation(equation) == ("NOT", 'x')
+
+
 def test_find_value_on_line():
     instructions = ['123 -> x', '456 -> y', 'x AND y -> d', 'x OR y -> e', 'x LSHIFT 2 -> f', 'y RSHIFT 2 -> g',
                     'NOT x -> h', 'NOT y -> i']
     wire_dictionary = part_1.create_dictionary(instructions)
     assert part_1.find_value_on_line(wire_dictionary, 'x') == 123
+    assert part_1.find_value_on_line(wire_dictionary, 'y') == 456
+    assert part_1.find_value_on_line(wire_dictionary, 'd') == 72
+    assert part_1.find_value_on_line(wire_dictionary, 'e') == 507
