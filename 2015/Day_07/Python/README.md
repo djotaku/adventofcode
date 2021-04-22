bee897ec4312d8e862e1c342d50c320393022076
Eric Mesa <ericsbinaryworld@gmail.com>
2021-04-22 18:25:09 -0400

    WOW! Memoization is like MAGIC!

--- 2015/Day_07/Python/README.md
+++ 2015/Day_07/Python/README.md
@@ -3,4 +3,17 @@
 ## Part 1
 - need to learn how to make Python just use a ushort so that it stays in the right range. Unfortunately my Python was compiled w/o the ability to use ctypes. Fortunately after trying different Google search terms for 2 days, I found that the answer is instead of using "~" to XOR it with 65535.
 - You almost certainly want to be consistent with your return types. It seems I was getting an error with my break_up_equation function because in the case of a straight through eg lx -> b I wasn't returning it as a list-type or some kind of iterable, so it was making the if/else on the find_value_on_line no go all the way through.
-- On 21 April as I was working on this problem, I finally [made a commit](https://github.com/djotaku/adventofcode/blob/375824bfe722085df8dc4b03c57c71e32b1855a6/2015/Day_07/Python/part_1.py) that passed all the tests, including the compliment. But 13 hours later (as I write this) it's still running. So while I got the recursion right, the solution is taking way longer than it should.
+- THE IMPORTANCE OF MEMOIZATION
+    - On 21 April as I was working on this problem, I finally [made a commit](https://github.com/djotaku/adventofcode/blob/375824bfe722085df8dc4b03c57c71e32b1855a6/2015/Day_07/Python/part_1.py) that passed all the tests, including the compliment. I started running it at 1901. Nearly 24 hours later 4/22 1822 - it was STILL running!
+    - Luckily I happened across someone else having issues with this and memoizaiton was recommended. I remember not being able to make use of it in 2020, but tried it again. I had to restructure my code a little - remove the dictionary from the "find_value_on_line" function. It then completed INSTANTLY!
+    
+```python
+$ python part_1_memoization.py                                                                                    
+Starting at 22-Apr-2021 18:19:07
+equation='lx'
+one went straight through
+The value on wire a is 956
+Ended at 22-Apr-2021 18:19:07
+```
+
+## Part 2
---
+++ 2015/Day_07/Python/part_1_memoization.py
@@ -0,0 +1,80 @@
+from datetime import datetime
+from functools import lru_cache
+import re
+import sys
+sys.path.insert(0, '../../input_parsing')
+import parse_input
+
+all_wires = {}
+
+
+def create_dictionary(instructions):
+    """Take in instructions and place the connections into a dictionary entry for the wire.
+    eg: 123 -> x would lead to a key of x and a value of 123.
+    """
+    wires = {}
+    for instruction in instructions:
+        pattern = re.compile(r'(.*) -> (\w+)')
+        regex = re.findall(pattern, instruction)
+        connection = regex[0][0]
+        wire = regex[0][1]
+        wires[wire] = connection
+    return wires
+
+
+def break_up_equation(equation):
+    """Figure out the operand(s) and operation and return them."""
+    two_operand_regex = re.compile(r'(\w+) ([A-Z]*) (\w+)')
+    one_operand_regex = re.compile('([A-Z]*) ([a-z]+)')
+    if re.match(two_operand_regex, equation):
+        result = re.findall(two_operand_regex, equation)
+        return result[0][0], result[0][1], result[0][2]
+    elif re.match(one_operand_regex, equation):
+        result = re.findall(one_operand_regex, equation)
+        return result[0][0], result[0][1]
+    else:
+        print(f"{equation=}")
+        return [equation]
+
+
+@lru_cache()
+def find_value_on_line(wire_to_find):
+    """Figure out what the final value is on a wire."""
+    if all_wires[wire_to_find].isnumeric():
+        return int(all_wires[wire_to_find])
+    equation = break_up_equation(all_wires[wire_to_find])
+    if len(equation) == 3:
+        if equation[0].isnumeric():
+            operand_left = int(equation[0])
+        else:
+            operand_left = find_value_on_line(equation[0])
+        if equation[2].isnumeric():
+            operand_right = int(equation[2])
+        else:
+            operand_right = find_value_on_line(equation[2])
+        operation = equation[1]
+        if operation == "AND":
+            return operand_left & operand_right
+        elif operation == "LSHIFT":
+            return operand_left << operand_right
+        elif operation == "OR":
+            return operand_left | operand_right
+        elif operation == "RSHIFT":
+            return operand_left >> operand_right
+    elif len(equation) == 2:
+        return find_value_on_line(equation[1]) ^ 65535
+    else:
+        print("one went straight through")
+        return find_value_on_line(equation[0])
+
+
+if __name__ == "__main__":
+    print(f"Starting at {datetime.now().strftime('%d-%b-%Y %H:%M:%S')}")
+    bobby_instructions = parse_input.input_per_line('../input.txt')
+    all_wires = create_dictionary(bobby_instructions)
+    wire_a = find_value_on_line('a')
+    print(f"The value on wire a is {wire_a}")
+    print(f"Ended at {datetime.now().strftime('%d-%b-%Y %H:%M:%S')}")
+
+# the answer is not -7074
+# 58462 is too high
