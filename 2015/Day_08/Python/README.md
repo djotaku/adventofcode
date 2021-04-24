3b5fa29c14d46cece2fbc6eb4db1ef30b8eec36a
Eric Mesa <ericsbinaryworld@gmail.com>
2021-04-24 18:25:42 -0400

    Day 8 Part 1 Python
    This one is kicking my butt!
    
    Documenting this before I get rid of what I think is some redundant code.

---
+++ 2015/Day_08/Python/README.md
@@ -0,0 +1,7 @@
+# What I learned about Pytyhon on Day 08
+
+## Part 1
+- I know I could have collapsed it to one function, but I did it this way for testing.
+- the test_input.txt was to see how string are interpreted when they are read in from a file.
+
+## Part 2
---
+++ 2015/Day_08/Python/test_part_1.py
@@ -0,0 +1,30 @@
+from . import part_1
+
+
+def test_string_code_length():
+    assert part_1.string_code_length(r'""') == 2
+    assert part_1.string_code_length(r'"abc"') == 5
+    assert part_1.string_code_length(r'"aaa\"aaa"') == 10
+    assert part_1.string_code_length(r'"\x27"') == 6
+
+
+def test_in_memory_strings():
+    assert part_1.in_memory_strings(r'""') == 0
+    assert part_1.in_memory_strings(r'"abc"') == 3
+    assert part_1.in_memory_strings(r'"aaa\"aaa"') == 7
+    assert part_1.in_memory_strings(r'"\x27"') == 1
+    assert part_1.in_memory_strings(r'"\xff"') == 1
+    assert part_1.in_memory_strings(r'"\xff\xca"') == 2
+    assert part_1.in_memory_strings(r'"\\\\"') == 2
+    assert part_1.in_memory_strings(r'"jcrkptrsasjp\\\"cwigzynjgspxxv\\vyb"') == 32
+    assert part_1.in_memory_strings(r'"oh\\x\\h"') == 6
+    assert part_1.in_memory_strings(r'"\xzz\xll"') == 8
+    assert part_1.in_memory_strings(r'"\\\"') == 2
+    assert part_1.in_memory_strings(r'"\\\""') == 2
+    assert part_1.in_memory_strings(r'"\\\xff\xca"') == 3
+    assert part_1.in_memory_strings(r'"\xzz\xzz"') == 8
+    assert part_1.in_memory_strings(r'"\\\"') == 2
+    assert part_1.in_memory_strings(r'"\\\\\\"') == 3
+    assert part_1.in_memory_strings(r'"\\\\\\\"') == 4
+    assert part_1.in_memory_strings(r'"\"\""') == 2
+    assert part_1.in_memory_strings(r'"\\"\""') == 3
