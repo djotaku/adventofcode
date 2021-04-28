e6e1e806e76514a709f97045a4f39a40e6ced943
Eric Mesa <ericsbinaryworld@gmail.com>
2021-04-27 21:06:17 -0400

    ready to implement the traveling salesman portion

---
+++ 2015/Day_09/Perl/README.md
@@ -0,0 +1,8 @@
+# What I learned on Day 09 in Perl
+
+## Part 1
+- how to create a subroutine
+- how to pass an array to a subroutine
+- constantly changing the symbol from @ or % to $ is annoying
+- ```my %index_dictionary = %city_dict_keys[0..$#city_dict_keys];``` aka Perl slices - requires v5.20
+- 
--- 2015/Day_09/Perl/part_1.pl
+++ 2015/Day_09/Perl/part_1.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 
-use v5.14;
+use v5.20;
 use warnings;
 
 sub parse_connections {
 
-    my $lines = @_[0];   # just doing this for a friendly name
+    my $lines = $_[0];   # just doing this for a friendly name
     my %hamilton_dict;
     foreach my $line (@$lines){
     
@@ -18,8 +18,57 @@ sub parse_connections {
     return %hamilton_dict;
 }
 
+sub create_matrix{
+
+    my %city_dict = %{$_[0]};
+    
+    my @city_dict_keys = (sort keys %city_dict);
+    
+    my %index_dictionary = %city_dict_keys[0..$#city_dict_keys];
+    
+    my @matrix;
+    
+    my $index_dictionary_size = keys %index_dictionary;
+    
+    for(my $first_number = 0; $first_number < $index_dictionary_size; $first_number++)
+    {
+        my @temp_internal_list;
+        
+        my $current_city = $index_dictionary{$first_number};
+        
+        for (my $second_number = 0; $second_number < $index_dictionary_size; $second_number++)
+        {
+        
+            if ($second_number == $first_number)
+            {
+            
+                push(@temp_internal_list, 0);
+            
+            }
+            
+            else
+            {
+            
+            push(@temp_internal_list, int($city_dict{$current_city}{$index_dictionary{$second_number}}));
+            
+            }
+        
+        }
+        push(@temp_internal_list, 0);
+        push(@matrix, \@temp_internal_list);
+    }
+    my @final_zeroes = (0) x ($index_dictionary_size + 1);
+    push(@matrix, \@final_zeroes);
+    
+    return @matrix;
+}
+
+
 my @full_set = ("London to Dublin = 464", "London to Belfast = 518", "Dublin to Belfast = 141");
 
 my %city_connection_hash = &parse_connections(\@full_set);
 
 say $city_connection_hash{"London"}{"Dublin"};
+
+my @city_matrix = create_matrix(\%city_connection_hash);
+
