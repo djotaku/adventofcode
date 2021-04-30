f5eb643adf50f146c7ecac2ec089d7f15e773815
Eric Mesa <ericsbinaryworld@gmail.com>
2021-04-29 20:11:50 -0400

    clearly the wrong approach

--- 2015/Day_10/Python/part_1.py
+++ 2015/Day_10/Python/part_1.py
@@ -1,10 +1,22 @@
 """Find length of the a number after a look-and-say game."""
-import re
+import copy
 
 
 def create_number_lists(game_input):
-    combined_list = []
+    number_list = []
+    temp_list = []
+    current_number = 0
     for number in game_input:
         if len(game_input) == 1:
-            return [number]
-
+            return [[1]]
+        else:
+            if int(number) == current_number:
+                temp_list.append(int(number))
+            else:
+                if temp_list:
+                    list_to_insert = copy.deepcopy(temp_list)
+                    number_list.append(list_to_insert)
+                current_number = int(number)
+                temp_list.append(int(number))
+    number_list.append(temp_list)
+    return number_list
--- 2015/Day_10/Python/test_part_1.py
+++ 2015/Day_10/Python/test_part_1.py
@@ -2,5 +2,6 @@ from . import part_1
 
 
 def test_create_number_lists():
-    assert part_1.create_number_lists("1") == (['1'])
-    assert part_1.create_number_lists("11") == (['1', '1'])
+    assert part_1.create_number_lists("1") == [[1]]
+    assert part_1.create_number_lists("11") == [[1, 1]]
+    assert part_1.create_number_lists("21") == [[2], [1]]
