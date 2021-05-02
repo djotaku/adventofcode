403583fd5850c579020ec41ac1b74e0ee462e356
Eric Mesa <ericsbinaryworld@gmail.com>
2021-05-02 18:20:45 -0400

    I believe it works for one round at this point

--- 2015/Day_10/part_1.rb
+++ 2015/Day_10/part_1.rb
@@ -1,2 +1,13 @@
-def create_number_list(game_input)
-    
+def look_and_say_round(game_input)
+    game_input_array = game_input.split(//)
+    game_input_array.chunk_while { |a, b| a == b }
+    .flat_map { |chunk| "#{chunk.size}#{chunk.first}" }
+    .join('')
+end
+
+
+if $PROGRAM_NAME == __FILE__
+    input = "1321131112"
+    puts "#{look_and_say_round(input)}"
+end
+
