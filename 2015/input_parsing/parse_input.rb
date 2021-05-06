a18e5df77687edbb2c7f1f0bab59834c5d2178f7
Eric Mesa <ericsbinaryworld@gmail.com>
2021-05-06 19:29:56 -0400

    Made the input parser WAAAAAY more Rubyist

--- 2015/input_parsing/parse_input.rb
+++ 2015/input_parsing/parse_input.rb
@@ -1,8 +1,3 @@
 def input_per_line(file)
-    lines = File.readlines(file)
-    cleaned_up_lines = []
-    for line in lines
-        cleaned_up_lines.append(line.strip)
-    end
-    return cleaned_up_lines
+  File.readlines(file, chomp:true) 
 end
