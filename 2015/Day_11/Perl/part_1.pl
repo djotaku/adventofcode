3c5a551debc8292c6dc7d701cb1d415aa3c03178
Eric Mesa <ericsbinaryworld@gmail.com>
2021-05-08 17:23:59 -0400

    rule one works in perl

---
+++ 2015/Day_11/Perl/part_1.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use v5.20;
+use warnings;
+use Array::GroupBy qw(igroup_by);
+
+sub rule_one{
+
+    my $password = $_[0];
+    
+    my @password_chars = split("", $password);
+    
+    my $rule_one_iterator = igroup_by(data => \@password_chars, compare => sub{++$_[0] eq $_[1]},);
+
+    while (my $group = $rule_one_iterator->())
+    {
+        my $length = @{$group};
+        if ($length >= 3)
+        {
+            return 1;
+        }
+    }
+    # if we got here, it didn't find a 3 character straight
+    return 0;
+}
+
+# Testing examples:
+say "TRUE!" if rule_one("hijklmmn");
+say "TRUE!" if rule_one("abbceffg");
