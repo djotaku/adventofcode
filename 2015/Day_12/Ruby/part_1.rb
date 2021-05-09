61e08b6444bd0c80e33f426d70e1a92d4f468103
Eric Mesa <ericsbinaryworld@gmail.com>
2021-05-09 11:03:00 -0400

    Part 1 done in Ruby

--- 2015/Day_12/Ruby/part_1.rb
+++ 2015/Day_12/Ruby/part_1.rb
@@ -1,15 +1,19 @@
 require "../../input_parsing/parse_input"
 
 def find_numbers(line)
-    line.scan(/(-*\d+)/)
+    numbers = line.scan(/(-*\d+)/).flat_map {|e| e}
 end
 
 def sum_numbers(numbers)
-    numbers.reduce(0) {|sum, num| sum + num}
+    numbers.reduce(0) {|sum, num| sum + num.to_i}
 end
 
 if $PROGRAM_NAME == __FILE__
    lines  = input_per_line('../input.txt')
    line_totals = []
-   
+   lines.each do | line |
+       line_totals.append(sum_numbers(find_numbers(line)))
+   end
+   final_total = line_totals.reduce(0) {|sum, num| sum + num}
+   puts "The Final sum of all the numbers is #{final_total}"
 end
