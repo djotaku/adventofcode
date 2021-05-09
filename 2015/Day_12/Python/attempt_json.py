f75b041fbbb19603e94121e26ed79de1e3e56d9c
Eric Mesa <ericsbinaryworld@gmail.com>
2021-05-09 17:12:50 -0400

    this is a god-forsaken mess
    This may need to be my first skip of 2015

--- 2015/Day_12/Python/attempt_json.py
+++ 2015/Day_12/Python/attempt_json.py
@@ -5,18 +5,33 @@ path.insert(0, '../../input_parsing')
 import parse_input
 
 
-def find_numbers(json_data: dict):
+def find_numbers(json_data):
     regex = re.compile(r'(-*\d+)')
+    number_list = []
     for json_list in json_data:
         if isinstance(json_list, dict):
-            for key, value in json_list.items():
-                print("it's a dictionary!")
-                print(f"{key=} and {value=}")
-                print('-----------------------------')
+            if "red" not in json_list.values():
+                for value in json_list.values():
+                    if isinstance(value, dict):
+                        find_numbers(value)
+                    elif isinstance(value, str):
+                        if regex.match(value):
+                            number_list.append(int(value))
+                    elif isinstance(value, list):
+                        for char in value:
+                            print(f"upper: {char=}")
+                            if regex.match(str(char)):
+                                number_list.append(int(char))
         else:
-            print("it's a list!")
-            print(json_list)
-            print('-----------------------------')
+            for inner_list in json_list:
+                if isinstance(inner_list, dict):
+                    find_numbers(inner_list)
+                else:
+                    for char in inner_list:
+                        print(f"lower: {char=}")
+                        if regex.match(char):
+                            number_list.append(int(char))
+    #print(number_list)
 
 
 lines = parse_input.input_per_line('../input.txt')
