a9e3a196825717a111a889939e449c68f9a83642
Eric Mesa <ericsbinaryworld@gmail.com>
2021-05-09 16:46:04 -0400

    advancing a bit on the json interpretation

--- 2015/Day_12/Python/attempt_json.py
+++ 2015/Day_12/Python/attempt_json.py
@@ -1,4 +1,5 @@
 import json
+import re
 from sys import path
 path.insert(0, '../../input_parsing')
 import parse_input
@@ -6,9 +7,18 @@ import parse_input
 
 def find_numbers(json_data: dict):
     regex = re.compile(r'(-*\d+)')
-    
+    for json_list in json_data:
+        if isinstance(json_list, dict):
+            for key, value in json_list.items():
+                print("it's a dictionary!")
+                print(f"{key=} and {value=}")
+                print('-----------------------------')
+        else:
+            print("it's a list!")
+            print(json_list)
+            print('-----------------------------')
 
 
 lines = parse_input.input_per_line('../input.txt')
 elf_json = json.loads(lines[0])
-print(elf_json)
+find_numbers(elf_json)
--- 2015/Day_12/Python/test_part_2.py
+++ 2015/Day_12/Python/test_part_2.py
@@ -6,7 +6,7 @@ def test_find_numbers():
     assert part_2.find_numbers('[1,{"c":"red","b":2},3]') == [1, 3]
     assert part_2.find_numbers('{"d":"red","e":[1,2,3,4],"f":5}') == []
     assert part_2.find_numbers('[1,"red",5]') == [1, 5]
-    assert part_2.find_numbers('{"d":["red", 4, 6]}') == [4, 6]  # should this test be passing?
+    assert part_2.find_numbers('{"d":["red", 4, 6]}') == [4, 6]
 
 
 def test_sum_number_list():
