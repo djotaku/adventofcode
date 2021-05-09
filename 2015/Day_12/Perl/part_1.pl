b350551ad97970d45b32642fa7496f209eabea90
Eric Mesa <ericsbinaryworld@gmail.com>
2021-05-09 11:25:33 -0400

    not quite working

---
+++ 2015/Day_12/Perl/part_1.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use v5.20;
+use warnings;
+
+open(CRAZYJS, '../input.txt') || die "NO INPUT!";
+
+my @lines = <CRAZYJS>; 
+
+my @numbers;
+
+for my $line (@lines){
+
+    my @line_numbers = $line =~ m/(-*\d+)/g;
+    push (@numbers, @line_numbers);
+}
+
+my $total = 0;
+
+for my $number_array (@numbers)
+{
+    for my $number (@{$number_array})
+    {
+        $total += $number;
+    }
+}
+
+say "The number total is $total";
