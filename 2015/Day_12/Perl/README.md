9cd1c12a4b55133e0172ddbe87e3bae4a7f73d8f
Eric Mesa <ericsbinaryworld@gmail.com>
2021-05-12 18:43:56 -0400

    Perl part 2 completed.

--- 2015/Day_12/Perl/README.md
+++ 2015/Day_12/Perl/README.md
@@ -4,3 +4,8 @@
 - Nothing
 
 ## Part 2
+- use Scalar::Util qw(looks_like_number) to figure out if something is a number
+- using ref($scalar) eq "ARRAY" or ref($scalar) eq "HASH" to tell if they are hashes
+- Apparently there's no way to tell if something is a string. If it's not a number, hash, or array - it's a string. So I ended up eliminating the string check that I had in Ruby and Python because it wasn't working well. 
+- the same way that you can do (keys %hash), you can also do (values %hash)
+- the JSON::PP library (which is built-in)
--- 2015/Day_12/Perl/part_2.pl
+++ 2015/Day_12/Perl/part_2.pl
@@ -1,13 +1,54 @@
 #!/usr/bin/perl
 
 use JSON::PP;
+use Scalar::Util qw(looks_like_number);
 use v5.20;
 use warnings;
 
+sub json_sums{
+    my $summation = 0;
+    my $json_item = $_[0];
+    if (ref($json_item) eq "ARRAY"){
+        for my $item (@{$json_item}){
+            $summation += json_sums($item);
+        }
+    }
+    elsif (looks_like_number($json_item)){
+        return $json_item;
+    }
+    elsif (ref($json_item) eq "HASH"){
+        my @values = (values %{$json_item});
+        for my $value (@values){
+            if ($value eq "red"){
+                return 0;
+            }
+        }
+        for my $value (@values){
+            if (looks_like_number($value)){
+                $summation += $value;
+            }
+            elsif (ref($value) eq "ARRAY"){
+                $summation += json_sums($value);
+            }
+            elsif (ref($value) eq "HASH"){
+                $summation += json_sums($value);
+            }
+            else{
+                $summation += 0;
+            }
+        }
+    }
+    return $summation;
+}
 
 open(ELFJSON, "../input.txt") || die "NO ELF JSON!";
 
 my $elf_json = decode_json <ELFJSON>;
 
-say "Array!" if ref($elf_json) eq 'ARRAY';
-say "Hash!" if ref($elf_json) eq 'HASH';
+# test
+# say "Array!" if ref($elf_json) eq 'ARRAY';
+# say "Hash!" if ref($elf_json) eq 'HASH';
+
+my $final_sum = json_sums($elf_json);
+
+say "The total is $final_sum";
