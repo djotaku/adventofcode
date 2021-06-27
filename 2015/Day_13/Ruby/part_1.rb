e471f4bb1beaa6a4cff2a4b5fd8bc600791cd3d7
Eric Mesa <ericsbinaryworld@gmail.com>
2021-06-26 22:32:16 -0400

    not yet working, but making a save point

---
+++ 2015/Day_13/Ruby/part_1.rb
@@ -0,0 +1,13 @@
+require "../../input_parsing/parse_input"
+
+def create_guest_hash(lines)
+    guest_hash = Hash.new
+    lines.each do | line |
+        people_and_values = line.scan(/(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)\./)
+        if !guest_hash.has_key?(people_and_values[0][0])
+            guest_hash[people_and_values[0][0]] = {}
+        end
+        number = "-{people_and_values[0][2]}" if people_and_values[0][1] == "lose" else people_and_values[0][2]
+        guest_hash[people_and_values[0][0]][people_and_values[0][3]] = number
+    end
+end
---
+++ 2015/Day_13/Ruby/test_part_1.rb
@@ -0,0 +1,12 @@
+require_relative "part_1"
+require 'test/unit'
+
+class TestGuestSeating < Test::Unit::TestCase
+
+    def test_create_guest_hash
+        sentence = ["Alice would gain 54 happiness units by sitting next to Bob."]
+        guest_hash = create_guest_hash(sentence)
+        assertequal("54", guest_hash['Alice']['Bob'])
+    end
+
+end
