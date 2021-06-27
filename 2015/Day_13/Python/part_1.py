3d4780060fe726b23df2f59eea3f97e3d19610fe
Eric Mesa <ericsbinaryworld@gmail.com>
2021-06-26 21:18:17 -0400

    not working, but I want to preserve this spot

--- 2015/Day_13/Python/part_1.py
+++ 2015/Day_13/Python/part_1.py
@@ -5,36 +5,6 @@ path.insert(0, '../../input_parsing')
 import parse_input
 
 
-def happy_seating_arrangement(graph, starting_person: int, number_of_people):
-    """For AoC implementing LONGEST path.
-
-    :param graph: A matrix of distances between cities. Add an extra row and column of 0s for shortest Hamilton Walk.
-    :param starting_person: Should be an the index of one of the cities.
-    :param number_of_people: The number of cities to visit. Add an extra one for Shortest Hamilton Walk.
-    """
-    # store all vertex apart from source vertex
-    vertex = [number for number in range(number_of_people) if number != starting_person]
-    # store minimum weight Hamiltonian Cycle
-    max_path = 0
-    next_permutation = permutations(vertex)
-    for permutation in next_permutation:
-
-        # store current Path weight(cost)
-        current_path_weight = 0
-
-        # compute current path weight
-        outer_array_index = starting_person
-        for inner_array_index in permutation:
-            current_path_weight += graph[outer_array_index][inner_array_index]
-            outer_array_index = inner_array_index
-        current_path_weight += graph[outer_array_index][starting_person]
-
-        # update minimum
-        max_path = max(max_path, current_path_weight)
-
-    return max_path
-
-
 def happy_seating_genetic_algorithm(seating_matrix, starting_person, number_of_people):
     # store all vertex apart from source vertex
     vertex = [number for number in range(number_of_people) if number != starting_person]
@@ -54,3 +24,13 @@ def happy_seating_genetic_algorithm(seating_matrix, starting_person, number_of_p
         max_happiness = max(max_happiness, current_happiness)
 
     return max_happiness
+
+
+def create_dictionary(sentence):
+    regex = re.compile(r'(\w+).+(gain|lose) (\d+).+(\w+)\.')
+    # regex = re.compile(r'(\w+)')
+    # regex = re.compile(r'(gain|lose)')
+    matches = re.findall(regex, sentence)
+    print(f'{matches=}')
+    number = -matches[0][2] if matches[0][1] == "lose" else matches[0][2]
+    return {matches[0][0]: {matches[0][3]: number}}
--- 2015/Day_13/Python/test_part_1.py
+++ 2015/Day_13/Python/test_part_1.py
@@ -1,7 +1,7 @@
 from . import part_1
 
 
-def test_happy_seating_arrangement():
+def test_happy_seating_genetic_algorithm():
     # Alice, Bob, Carol, Dave
     seating_matrix = [[0, 54, -79, -2],
                       [83, 0, -7, -63],
@@ -9,3 +9,12 @@ def test_happy_seating_arrangement():
                       [46, -7, 41, 0]]
     starting_person = 0
     assert part_1.happy_seating_genetic_algorithm(seating_matrix, starting_person, len(seating_matrix)) == 330
+
+
+def test_create_dictionary():
+    sentence = "Alice would gain 54 happiness units by sitting next to Bob."
+    dictionary = part_1.create_dictionary(sentence)
+    assert dictionary["Alice"]["Bob"] == 54
+    sentence = "Alice would lose 79 happiness units by sitting next to Carol."
+    dictionary = part_1.create_dictionary(sentence)
+    assert dictionary["Alice"]["Carol"] == -79
