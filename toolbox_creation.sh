66ab926acab65f808be4b24ac1e9e651eb2da199
Eric Mesa <ericsbinaryworld@gmail.com>
2021-08-20 18:30:19 -0400

    toolbox scripts

---
+++ toolbox_creation.sh
@@ -0,0 +1,2 @@
+echo "Creating a new toolbox for Advent of Code Development"
+toolbox create adventofcode
---
+++ toolbox_setup.sh
@@ -0,0 +1,7 @@
+echo "Run me inside the toolbox: toolbox enter adventofcode"
+
+echo "Install perl"
+sudo dnf install perl -y
+
+echo "Install CPAN Minus"
+sudo cpan App:cpanminus
