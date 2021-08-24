753c1eb40f68d494fd7586443b86ad26f32b6aa9
Eric Mesa <ericsbinaryworld@gmail.com>
2021-08-23 20:26:17 -0400

    I can now parse the ingredients

--- 2015/Day_15/Python/part_1.py
+++ 2015/Day_15/Python/part_1.py
@@ -1,4 +1,5 @@
 from sys import path
+import re
 path.insert(0, '../../input_parsing')
 import parse_input
 
@@ -8,11 +9,20 @@ def ingredient_score(teaspoon_list, ingredient_list):
     total_ingredients = []
     for multiplication_tuple in combined_list:
         temp_list = [
-            item * multiplication_tuple[0] for item in multiplication_tuple[1]
+            int(item) * multiplication_tuple[0] for item in multiplication_tuple[1]
         ]
         total_ingredients.append(temp_list)
     properties = zip(*total_ingredients)
     final_score = 1
+    property_count = 1  # this is to ignore calories for part 1
     for cookie_property in properties:
         final_score *= sum(cookie_property)
+        property_count += 1
+        if property_count == 5:
+            break
     return final_score
+
+
+def parse_ingredients(ingredient_inputs):
+    pattern = re.compile(r'(-*\d)')
+    return [re.findall(pattern, ingredient) for ingredient in ingredient_inputs]
--- 2015/Day_15/Python/test_part_1.py
+++ 2015/Day_15/Python/test_part_1.py
@@ -2,7 +2,15 @@ from . import part_1
 
 
 def test_total_ingredients():
-    butterscotch = [-1, -2, 6, 3]
-    cinnamon = [2, 3, -2, -1]
+    butterscotch = ['-1', '-2', '6', '3', '8']
+    cinnamon = ['2', '3', '-2', '-1', '3']
     cookie_score = part_1.ingredient_score([44, 56], [butterscotch, cinnamon])
     assert cookie_score == 62842880
+
+
+def test_parse_ingredients():
+    butterscotch = ['-1', '-2', '6', '3', '8']
+    cinnamon = ['2', '3', '-2', '-1', '3']
+    ingredient_inputs = ["Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8",
+                         "Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"]
+    assert part_1.parse_ingredients(ingredient_inputs) == [butterscotch, cinnamon]
