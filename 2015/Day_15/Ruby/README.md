a6da23be91e01b6f9971e26a5747a7375d1a3360
Eric Mesa <ericsbinaryworld@gmail.com>
2021-08-24 19:01:02 -0400

    Gets the correct answer. Takes WAAAAAAAAAY longer than in Python

---
+++ 2015/Day_15/Ruby/README.md
@@ -0,0 +1,7 @@
+# What I learned about Ruby 
+
+## Part 1
+- How to do permutations in Ruby
+- Permutations in Ruby are WAAAAAAAAAY slower than in Python
+- getting better at using map where I'd use list comprehensions in Python
+- 
---
+++ 2015/Day_15/Ruby/part_1.rb
@@ -0,0 +1,47 @@
+require "../../input_parsing/parse_input"
+
+def ingredient_score(multipliers, cookies)
+  intermediate = multipliers.zip(cookies)
+  product = []
+  intermediate.each do |multiplication_item|
+  temp_list = multiplication_item[1].map{|x| multiplication_item[0] * x.to_i}
+  product.append(temp_list)
+  temp_list = []
+  end
+  properties = product.transpose
+  final_score = 1
+  property_count = 1
+  properties.each do |cookie_property|
+    if cookie_property.sum > 0
+      final_score *= cookie_property.sum
+    end
+  property_count += 1
+  if property_count == 5
+    break
+  end
+  end
+  final_score
+end
+
+def parse_ingredients(ingredient_input)
+  ingredient_input.map{|x| x.scan(/-*\d/)}
+end
+
+def brute_force_cooke_score(ingredient_list)
+  ingredient_combos = (1..100).to_a.permutation(ingredient_list.length).map{|x| x if x.sum == 100}.compact
+  score = 0
+  ingredient_combos.each do |ingredient_combination|
+    combo_score = ingredient_score(ingredient_combination, ingredient_list)
+    if combo_score > score
+      score = combo_score
+    end
+  end
+  score
+end
+
+if $PROGRAM_NAME == __FILE__
+  cookie_list = input_per_line('../input.txt')
+  ingredients = parse_ingredients(cookie_list)
+  cookie_score = brute_force_cooke_score(ingredients)
+  puts "The cookie score is #{cookie_score}"
+end
