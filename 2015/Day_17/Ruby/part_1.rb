0af540e413a865dc2bdf347a1e8d513918eb67fc
Eric Mesa <ericsbinaryworld@gmail.com>
2021-08-28 19:23:48 -0400

    2015 Day 17 Ruby
    Definitely not working

--- 2015/Day_17/Ruby/part_1.rb
+++ 2015/Day_17/Ruby/part_1.rb
@@ -3,10 +3,6 @@ require "../../input_parsing/parse_input"
 container_sizes = input_per_line("../input.txt")
 container_combinations = []
 container_sizes = container_sizes.map{|x| x.to_i}
-(1..container_sizes.length).each do|x|
-  container_combinations.append(container_sizes.combination(x)
-                                 .map{|y| y if y.sum == 150}
-                                 .compact)
-end
-container_combinations = container_combinations.compact.reject(&:empty?)
-print container_combinations.length
+container_combinations = (1..container_sizes.length).to_a.each{|x| container_sizes.combination(x)}.map{|y| y if y.sum == 150}
+#container_combinations = container_combinations.compact.reject(&:empty?)
+print container_combinations
