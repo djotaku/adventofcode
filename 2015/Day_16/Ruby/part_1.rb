8f7e8977dac61b94293d06884e98c506e166bfbc
Eric Mesa <ericsbinaryworld@gmail.com>
2021-08-27 21:07:51 -0400

    Ruby Day 16 Part 1 Works
    In what I'm sure is definitely the wrong way to do this. Or at least the least elegant!

--- 2015/Day_16/Ruby/part_1.rb
+++ 2015/Day_16/Ruby/part_1.rb
@@ -4,7 +4,7 @@ def create_sue_hash(sue_value_list)
   sue_hash = Hash.new
   sue_value_list.each do |aunt|
     important_values = aunt.scan(/Sue (\d*): (\w*): (\d*), (\w*): (\d*), (\w*): (\d*)/)
-    sue_hash = {important_values[0][0]=>{important_values[0][1]=>important_values[0][2],important_values[0][3]=>important_values[0][4], important_values[0][5]=>important_values[0][6]}}
+    sue_hash[important_values[0][0]] = {important_values[0][1]=>important_values[0][2],important_values[0][3]=>important_values[0][4], important_values[0][5]=>important_values[0][6]}
   end
   sue_hash
 end
@@ -13,6 +13,15 @@ end
 if $PROGRAM_NAME == __FILE__
   sue_list = input_per_line('../input.txt')
   sue_hash = create_sue_hash(sue_list)
-  puts sue_hash
+  sue_hash.keep_if{|key, value| value["children"] == '3' or value["children"] == nil}
+  sue_hash.keep_if{|key, value| value["cats"] == '7' or value["cats"] == nil }
+  sue_hash.keep_if{|key, value| value["samoyeds"] == '2' or value["samoyeds"] == nil }
+  sue_hash.keep_if{|key, value| value["pomeranians"] == '3' or value["pomeranians"] == nil }
+  sue_hash.keep_if{|key, value| value["akitas"] == '0' or value["akitas"] == nil }
+  sue_hash.keep_if{|key, value| value["vizslas"] == '0' or value["vizslas"] == nil }
+  sue_hash.keep_if{|key, value| value["goldfish"] == '5' or value["goldfish"] == nil }
+  sue_hash.keep_if{|key, value| value["trees"] == '3' or value["trees"] == nil }
+  sue_hash.keep_if{|key, value| value["cars"] == '3' or value["cars"] == nil }
+  sue_hash.keep_if{|key, value| value["perfumes"] == '3' or value["perfumes"] == nil }
   puts sue_hash
 end
