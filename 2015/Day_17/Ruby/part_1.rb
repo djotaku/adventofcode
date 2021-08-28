9117aa6470f43f3b05bcb9b7799a2e52572cb1d0
Eric Mesa <ericsbinaryworld@gmail.com>
2021-08-28 19:20:15 -0400

    2015 Day 17 Ruby
    not quite working, but want to save this state

---
+++ 2015/Day_17/Ruby/part_1.rb
@@ -0,0 +1,12 @@
+require "../../input_parsing/parse_input"
+
+container_sizes = input_per_line("../input.txt")
+container_combinations = []
+container_sizes = container_sizes.map{|x| x.to_i}
+(1..container_sizes.length).each do|x|
+  container_combinations.append(container_sizes.combination(x)
+                                 .map{|y| y if y.sum == 150}
+                                 .compact)
+end
+container_combinations = container_combinations.compact.reject(&:empty?)
+print container_combinations.length
