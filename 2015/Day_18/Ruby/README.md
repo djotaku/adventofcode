07a8308645fc515dee03f1017ecc69ecc685c08a
Eric Mesa <ericsbinaryworld@gmail.com>
2021-08-30 20:00:44 -0400

    2015 Day 18 Ruby Part 2 Completed!

--- 2015/Day_18/Ruby/README.md
+++ 2015/Day_18/Ruby/README.md
@@ -2,3 +2,7 @@
 
 ## Part 1
 - If you want to do x<=y<=z you would do (x..z).cover? y
+- Interesting that, by default, the Hash just returns nil instead of erroring out if I try to access a key that doesn't exist. Easier than Python's having to to use "get".
+
+## Part 2
+- 
---
+++ 2015/Day_18/Ruby/part_2.rb
@@ -0,0 +1,78 @@
+require "../../input_parsing/parse_input"
+
+def initialize_board(list_of_rows)
+  board = Hash.new
+  list_of_rows.each_with_index do |row, row_index |
+    columns = row.split(//)
+    columns.each_with_index do |column, column_index|
+      if column == "#"
+        board[[row_index, column_index]] = 1
+      end
+    end
+  end
+  board
+end
+
+def new_status(coordinate, board)
+  live_neighbors = 0
+  alive = board[coordinate] == 1 ? true : false
+  if board[[coordinate[0]-1, coordinate[1]-1]] == 1
+    live_neighbors += 1
+  end
+  if board[[coordinate[0], coordinate[1]-1]] == 1
+    live_neighbors += 1
+  end
+  if board[[coordinate[0]+1, coordinate[1]-1]] == 1
+    live_neighbors += 1
+  end
+  if board[[coordinate[0]+1, coordinate[1]]] == 1
+    live_neighbors += 1
+  end
+  if board[[coordinate[0]+1, coordinate[1]+1]] == 1
+    live_neighbors += 1
+  end
+  if board[[coordinate[0], coordinate[1]+1]] == 1
+    live_neighbors += 1
+  end
+  if board[[coordinate[0]-1, coordinate[1]+1]] == 1
+    live_neighbors += 1
+  end
+  if board[[coordinate[0]-1, coordinate[1]]] == 1
+    live_neighbors += 1
+  end
+  if alive
+    if (2..3).cover? live_neighbors
+      return 1
+    else
+      return 0
+    end
+  end
+  if not alive and live_neighbors == 3
+    1
+  else
+    0
+  end
+end
+
+def play_round(grid_size, board)
+  new_board = Hash.new
+  (0...grid_size).each do |x|
+    (0...grid_size).each do |y|
+      new_board[[x, y]] = new_status([x,y], board)
+    end
+  end
+  # turn on the four corners
+  new_board[[0,0]] = 1
+  new_board[[0,99]] = 1
+  new_board[[99,0]] = 1
+  new_board[[99,99]] = 1
+  new_board
+end
+
+if $PROGRAM_NAME == __FILE__
+  day_18_input = input_per_line('../input.txt')
+  initial_board = initialize_board(day_18_input)
+  board = initial_board.compact
+  (1..100).each {board = play_round(100, board).compact}
+  puts "There are #{board.values.sum} lights on."
+end
