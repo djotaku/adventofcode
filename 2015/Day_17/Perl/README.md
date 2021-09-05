89f4f05d95f200e15c899821f541ed87b9f5f001
Eric Mesa <ericsbinaryworld@gmail.com>
2021-09-05 17:09:40 -0400

    Changes for 2015 Day 15 Part 1 Perl

---
+++ 2015/Day_17/Perl/README.md
@@ -0,0 +1,8 @@
+# What I learned from Perl
+
+## Part 1
+- Had to go from Algorithm::Permute to Algorithm::Combinatorics qw(combinations) because it was otherwise double-counting.
+- Combinatorics runs faster than Permute. (Or at least appears to)
+
+## Part 2
+- 
--- 2015/Day_17/Perl/part_1.pl
+++ 2015/Day_17/Perl/part_1.pl
@@ -4,12 +4,17 @@ use v5.20;
 use warnings;                                                                                  
 use List::Util qw/sum/;
 use  Data::Dump qw/dump/; 
-use Algorithm::Permute;
+use Algorithm::Combinatorics qw(combinations);
 
 
 open(CONTAINER_SIZES, "../input.txt") || die "No containers!";
 
 my @container_sizes = <CONTAINER_SIZES>;
+# my @container_sizes = (20, 15, 10, 5, 5);
+# my @container_sizes = ("20\n", "15\n", "10\n", "5\n", "5\n");
+
+
+my $CONTAINER_SUM = 150;
 
 my @all_ways;
 
@@ -18,16 +23,16 @@ my $size_all_ways = scalar @container_sizes;
 foreach my $number (1.. $size_all_ways)
 {
 	say $number;
-	my $permutations = Algorithm::Permute->new(\@container_sizes, $number);
-	while (my @combination = $permutations->next)
+	my $permutations = combinations(\@container_sizes, $number);
+	while (my $combination = $permutations->next)
 	{
 		say "A combination";
-		dump(@combination);
-		if(sum(@combination) == 150)
+		dump(@$combination);
+		if(sum(@$combination) == $CONTAINER_SUM)
 		{
-			push @all_ways, \@combination;
+			push @all_ways, \@$combination;
 		}	
-	#dump(@all_ways);
+	# dump(@all_ways);
 	say "size " . scalar @all_ways;
 	say "----------";
 	}
