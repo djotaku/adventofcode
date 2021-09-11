c7ae2c255f7c741c04356b83175d22151b601261
Eric Mesa <ericsbinaryworld@gmail.com>
2021-09-11 13:23:58 -0400

    Perhaps on the right track
    Want to save this situation

--- 2015/Day_19/Ruby/README.md
+++ 2015/Day_19/Ruby/README.md
@@ -1,5 +1,7 @@
 # What I learned in Ruby
 
 ## Part 1
+- getting better at using map (in this case map!) to do the equivalent of Python's list comprehensions. It took me a while, but I'm particularly proud of how the generate_molecule_tuple turned out. 
+- using str.to_enum(:scan, regular expression).map { Regexp.last_match} to do the equivalent of Python's re.finditer
 
 ## Part 2
--- 2015/Day_19/Ruby/part_1.rb
+++ 2015/Day_19/Ruby/part_1.rb
@@ -6,10 +6,17 @@ def generate_molecule_tuple(list_of_molecules)
   list_of_molecules.map! {|line| [line.match(/(\w+) => (\w+)/).to_a[1], line.match(/(\w+) => (\w+)/).to_a[2]]}
 end
 
+def generate_replacements(string_to_replace, replace_with_string, molecule)
+  matches = molecule.to_enum(:scan, string_to_replace).map { Regexp.last_match}
+  matches.map{|molecule_match| molecule_match.pre_match + replace_with_string + molecule_match.post_match}
+end
 
 if $PROGRAM_NAME == __FILE__
   list_of_molecules = input_per_line('../input.txt')
   molecule_to_change = list_of_molecules.pop
   list_of_molecules.pop
-  print generate_molecule_tuple(list_of_molecules)
+  molecule_tuples = generate_molecule_tuple(list_of_molecules)
+  list_of_potential_replacements = molecule_tuples.map {|item| generate_replacements(item[0], item[1],
+                                                                                      molecule_to_change)}
+  print list_of_potential_replacements
 end
