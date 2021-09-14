9c5c0880a96db4e78b2c36869f5aa6ef466a7b79
Eric Mesa <ericsbinaryworld@gmail.com>
2021-09-14 16:26:39 -0400

    updating the directory completion script

--- helper_scripts/create_directories.sh
+++ helper_scripts/create_directories.sh
@@ -5,5 +5,12 @@
 for num in {01..25}
 
 do
-echo $num
+mkdir "$HOME/Programming Projects/adventofcode/2016/Day_$num"
+cp "$HOME/Programming Projects/adventofcode/helper_scripts/daily_readme_template.md" "$HOME/Programming Projects/adventofcode/2016/Day_$num/README.md"
+git add "$HOME/Programming Projects/adventofcode/2016/Day_$num/README.md"
+mkdir "$HOME/Programming Projects/adventofcode/2016/Day_$num/Python"
+cp "$HOME/Programming Projects/adventofcode/helper_scripts/language_readme_template.md" "$HOME/Programming Projects/adventofcode/2016/Day_$num/Python/README.md"
+mkdir "$HOME/Programming Projects/adventofcode/2016/Day_$num/Ruby"
+cp "$HOME/Programming Projects/adventofcode/helper_scripts/language_readme_template.md" "$HOME/Programming Projects/adventofcode/2016/Day_$num/Ruby/README.md"
+
 done 
