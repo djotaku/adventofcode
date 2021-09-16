97d02ec61984e9584ea6fd0f3bbfa356a41676c9
Eric Mesa <ericsbinaryworld@gmail.com>
2021-09-15 21:05:42 -0400

    2015 Day 20 Part 1 Ruby Completed
    And converted to the more Ruby idiomatic .each instead of for loop

--- 2015/Day_20/Ruby/part_1.rb
+++ 2015/Day_20/Ruby/part_1.rb
@@ -2,16 +2,15 @@ PRESENTS_DELIVERED = 34000000
 
 house_list = Array.new((PRESENTS_DELIVERED/10), 0)
 
-for i in (1...(PRESENTS_DELIVERED/10)) do
-    for j in (i...(PRESENTS_DELIVERED/10)).step(i) do
+(1...(PRESENTS_DELIVERED / 10)).each { |i|
+    (i...(PRESENTS_DELIVERED / 10)).step(i).each { |j|
         house_list[j] += i * 10
-    end
-end
+    }
+}
 
-
-for house in house_list
+house_list.each_with_index { |house, house_number|
     if house >= PRESENTS_DELIVERED
-        puts "House is #{house}"
+        puts "House is #{house_number}"
         break
     end
-end
+}
