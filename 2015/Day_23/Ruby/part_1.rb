534fbf7281a934acb6e7d6a373e74f30bca17c62
Eric Mesa <ericsbinaryworld@gmail.com>
2021-09-19 16:39:34 -0400

    2015 Day 23 Ruby Part 1 Completed

---
+++ 2015/Day_23/Ruby/part_1.rb
@@ -0,0 +1,86 @@
+require "../../input_parsing/parse_input"
+
+assembly = input_per_line('../input.txt')
+pointer = 0
+register_a = 0
+register_b = 0
+while pointer < assembly.length
+  current_instruction = assembly[pointer].split
+  print current_instruction
+  print "\n"
+  puts register_a
+  puts register_b
+  #print "\n"
+  if current_instruction.length ==2
+    case current_instruction[0]
+    when "hlf"
+      case current_instruction[1]
+      when "a"
+        register_a = register_a / 2
+        pointer += 1
+      when "b"
+        register_b = register_b / 2
+        pointer += 1
+      end
+    when "tpl"
+      case current_instruction[1]
+      when "a"
+        register_a *= 3
+        pointer += 1
+      when "b"
+        register_b += 3
+        pointer += 1
+      end
+    when "inc"
+      case current_instruction[1]
+      when "a"
+        register_a += 1
+        pointer += 1
+      when "b"
+        register_b += 1
+        pointer += 1
+      end
+    when "jmp"
+      pointer += current_instruction[1].to_i
+    end
+  else
+    case current_instruction[0]
+    when "jie"
+      register_to_check = current_instruction[1].chop
+      case register_to_check
+      when "a"
+        if register_a % 2 == 0
+          pointer += current_instruction[2].to_i
+        else
+          pointer += 1
+        end
+      when "b"
+        if register_b % 2 == 0
+          pointer += current_instruction[2].to_i
+        else
+          pointer += 1
+      end
+      end
+    when "jio"
+      register_to_check = current_instruction[1].chop
+      case register_to_check
+      when "a"
+        if register_a == 1
+          pointer += current_instruction[2].to_i
+        else
+          pointer += 1
+        end
+      when "b"
+        if register_b == 1
+          pointer += current_instruction[2].to_i
+        else
+          pointer += 1
+        end
+      end
+    end
+  end
+end
+
+
+
+puts "The value in register b is #{register_b}"
