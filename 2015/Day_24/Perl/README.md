820d383ed250daa4b7952517c9e829427ca352b3
Eric Mesa <ericsbinaryworld@gmail.com>
2021-09-22 19:30:52 -0400

    2015 Day 24 Part 1 Perl

---
+++ 2015/Day_24/Perl/README.md
@@ -0,0 +1,5 @@
+# What I learned from this day's problem
+
+## Part 1
+- the contortions I had to go around for product to work again make me hate Perl. I think I'll probably come across the same thing with C and dereferencing pointers. It reminds me of that kind of stuff.
+## Part 2
--- 2015/Day_24/Perl/part_1.pl
+++ 2015/Day_24/Perl/part_1.pl
@@ -2,7 +2,63 @@
 
 use v5.20;                                                                                     
 use warnings;                                                                                  
-use List::Util qw/sum/;
+use List::Util qw/sum product min/;
 use  Data::Dump qw/dump/; 
 use Algorithm::Combinatorics qw(combinations);
+
+open(PACKAGES, "../input.txt") || die "No packages";
+
+my @packages = <PACKAGES>;
+#my @packages = (1,2,3,4,5,7,8,9,10,11);
+
+my $weight_per_section = (sum (@packages)/3);
+
+my @package_combinations;
+
+my $number_of_packages = scalar @packages;
+
+foreach my $number (1.. $number_of_packages)
+{
+	my $permutations = combinations(\@packages, $number);
+	while (my $combination = $permutations->next)
+	{
+		if(sum(@$combination) == $weight_per_section)
+		{
+			push @package_combinations, \@$combination;
+		}	
+	}
+}
+
+my $santa_leg_room_packages = 100000000000000000;
+
+foreach my $package_combo (@package_combinations)
+{
+    $santa_leg_room_packages = min ((scalar @$package_combo), $santa_leg_room_packages);
+}
+
+my @group_1_contenders;
+
+foreach my $package_combo (@package_combinations)
+{
+    if ((scalar @$package_combo) == $santa_leg_room_packages)
+    {
+        push @group_1_contenders, \@$package_combo;
+    }
+}
+
+
+if (scalar @group_1_contenders == 1)
+{
+    my $quantum_entanglement = product @{$group_1_contenders[0]};
+    say "The quantum entanglement of the packages in Santa's leg area is: $quantum_entanglement";
+}
+else
+{
+    my $quantum_entanglement = 10000000000000000000000;
+    foreach my $presents (@group_1_contenders)
+    {
+        $quantum_entanglement = min ($quantum_entanglement, (product @{$presents}))
+    }
+    say "The quantum entanglement of the packages in Santa's leg area is: $quantum_entanglement";
+}
 
