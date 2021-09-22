bb90b88ab3e891d4fedfb2e79a5191607c9ccaf8
Eric Mesa <ericsbinaryworld@gmail.com>
2021-09-22 15:05:03 -0400

    initial setup

---
+++ 2015/Day_24/Perl/part_1.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+use v5.20;                                                                                     
+use warnings;                                                                                  
+use List::Util qw/sum/;
+use  Data::Dump qw/dump/; 
+use Algorithm::Combinatorics qw(combinations);
+
