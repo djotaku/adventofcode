e9f0ffc07bb430e569da1e45b00fb72c176128ca
Eric Mesa <ericsbinaryworld@gmail.com>
2021-10-03 16:18:36 -0400

    getting ready for 2016

---
+++ 2016/Day_18/Go/README.md
@@ -0,0 +1,5 @@
+# What I learned from this day's problem
+
+## Part 1
+
+## Part 2
--- helper_scripts/create_directories.py
+++ helper_scripts/create_directories.py
@@ -8,13 +8,13 @@ PROGRAMMING_LANGUAGES = ["Python", "Ruby", "Perl", "Haskel", "Go"]
 
 def create_advent_of_code_setup():
     for day in range(1, 26):
-        os.mkdir(f"{HOME}/Programming Projects/adventofcode/Day_{day}")
+        os.mkdir(f"{HOME}/Programming Projects/adventofcode/{YEAR}/Day_{day:02d}")
         shutil.copyfile(f"{HOME}/Programming Projects/adventofcode/helper_scripts/daily_readme_template.md",
-                        f"{HOME}/Programming Projects/adventofcode/Day_{day}/README.md")
+                        f"{HOME}/Programming Projects/adventofcode/{YEAR}/Day_{day:02d}/README.md")
         for programming_language in PROGRAMMING_LANGUAGES:
-            os.mkdir(f"{HOME}/Programming Projects/adventofcode/Day_{day}/{programming_language}")
+            os.mkdir(f"{HOME}/Programming Projects/adventofcode/{YEAR}/Day_{day:02d}/{programming_language}")
             shutil.copyfile(f"{HOME}/Programming Projects/adventofcode/helper_scripts/language_readme_template.md",
-                            f"{HOME}/Programming Projects/adventofcode/Day_{day}/{programming_language}/README.md")
+                            f"{HOME}/Programming Projects/adventofcode/{YEAR}/Day_{day:02d}/{programming_language}/README.md")
 
 
 if __name__ == "__main__":
