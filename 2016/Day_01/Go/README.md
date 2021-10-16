182258cec801910c99356a1b53496e9a9ba075ab
Eric Mesa <ericsbinaryworld@gmail.com>
2021-10-16 17:56:10 -0400

    2016 Day 1 Go
    Looks like I'm going to have to go back to a map.

--- 2016/Day_01/Go/README.md
+++ 2016/Day_01/Go/README.md
@@ -1,5 +1,8 @@
 # What I learned from this day's problem
 
 ## Part 1
-
+- First go program ever other than Hello World type program
+- Learned how to use arrays and slices
+- how to do regular expressions in Go. For the compile want to use back-tick
+- strconv.Atoi to make strings into integers
 ## Part 2
--- 2016/Day_01/Go/solution.go
+++ 2016/Day_01/Go/solution.go
@@ -4,24 +4,56 @@ package main
 import (
 	"adventofcode/2016/aocinputs"
 	"fmt"
+	"math"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
 func main() {
 
-	direction := [2]float64{0, 1}
-	myLocation := [2]float64{0, 0}
-	visitedLocations := map[string]float64{"0,0": 1}
+	direction := [2]int{0, 1}
+	myLocation := [2]int{0, 0}
+	var visitedLocations [][]int
+	var partTwoLocation [2]int
 	continueToLookForDuplicates := true
 
-	directionInput, err := aocinputs.OneLine("../input.txt")
+	directionInput, err := aocinputs.OneLine("/home/ermesa/Programming Projects/adventofcode/2016/Day_01/input.txt")
 	if err != nil {
 		print(err)
 	}
 	directions := strings.Split(directionInput, ",")
+	fmt.Println(directions)
 	for _, value := range directions {
-		// start https://pkg.go.dev/regexp when you get back to this
+		fmt.Println(value)
+		re := regexp.MustCompile(`(\w)(\d+)`)
+		matches := re.FindStringSubmatch(value)
+		fmt.Printf("matches: %v\n", matches)
+		if matches[1] == "L" {
+			tempX := direction[0]
+			direction[0] = -direction[1]
+			direction[1] = tempX
+		} else {
+			tempX := direction[0]
+			direction[0] = direction[1]
+			direction[1] = -tempX
+		}
+		distanceToTravel, _ := strconv.Atoi(matches[2])
+		for step := 0; step < distanceToTravel; step++ {
+			myLocation[0] += direction[0]
+			myLocation[1] += direction[1]
+			if visitedLocations[myLocation[0]][myLocation[1]] == 0 && continueToLookForDuplicates {
+				partTwoLocation[0] = myLocation[0]
+				partTwoLocation[1] = myLocation[1]
+				continueToLookForDuplicates = false
+			} else {
+				visitedLocations[myLocation[0]][myLocation[1]] = 1
+			}
+		}
 	}
 
-	fmt.Println("hi")
+	partOneAnswer := math.Abs(float64(myLocation[0])) + math.Abs(float64(myLocation[1]))
+	partTwoAnswer := math.Abs(float64(partTwoLocation[0])) + math.Abs(float64(partTwoLocation[1]))
+	fmt.Printf("According to part 1's instructions. Easter Bunny HQ is %f blocks away.\n", partOneAnswer)
+	fmt.Printf("According to part 2's instructions. Easter Bunny HQ is %f blocks away.\n", partTwoAnswer)
 }
