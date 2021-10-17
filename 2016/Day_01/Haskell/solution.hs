3de1834ae6cd53a0ca80d1511e6011dbacbcdcf1
Eric Mesa <ericsbinaryworld@gmail.com>
2021-10-16 21:01:55 -0400

    2016 Day 01 Haskell solution is coming along

--- 2016/Day_01/Haskell/solution.hs
+++ 2016/Day_01/Haskell/solution.hs
@@ -1,6 +1,15 @@
 import Data.Complex
+import qualified Data.Text as T
 
 figureOutDirection :: Num a => ([Char], b) -> (Complex a, b)
 figureOutDirection (dir, distance)
     | dir == "L" = ((0:+1), distance)
     | otherwise = ((0:+ (-1)), distance)
+
+
+splitDirDistance x = (head x, tail x)
+
+splitStringOnCommas :: String -> [String]
+splitStringOnCommas x = map T.unpack (map T.strip (T.split (==',') (T.pack x)))
+    
+
