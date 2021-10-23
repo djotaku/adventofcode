d03e189cde09e380a4cf5418ba9803f5696f6e2b
Eric Mesa <ericsbinaryworld@gmail.com>
2021-10-22 20:08:23 -0400

    2016 Day 01 Haskell Part 1
    So close, but not yet there

--- 2016/Day_01/Haskell/solution.hs
+++ 2016/Day_01/Haskell/solution.hs
@@ -10,8 +10,13 @@ figureOutDirection (dir, distance)
 splitDirDistance :: [a] -> (a, [a])
 splitDirDistance x = (head x, tail x)
 
+fixString x = (fst x, read (snd x):: Float)
+
 splitStringOnCommas :: String -> [String]
 splitStringOnCommas x = map T.unpack (map T.strip (T.split (==',') (T.pack x)))
-    
-getComplexDirections :: Num a => String -> [(Complex a, [Char])]
-getComplexDirections x = map figureOutDirection ((map splitDirDistance (splitStringOnCommas x)))
+
+getComplexDirections :: Num a => String -> [(Complex a, Float)]
+getComplexDirections x = map figureOutDirection (map fixString ((map splitDirDistance (splitStringOnCommas x))))
+
+finalAnswer :: String -> [Complex Float]
+finalAnswer j = scanl (\x (y, z) -> x * y) (0:+1) (getComplexDirections j)
