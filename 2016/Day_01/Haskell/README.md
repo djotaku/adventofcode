cc482523bcad9c065b9ca2905c2617d49d144ec2
Eric Mesa <ericsbinaryworld@gmail.com>
2021-10-29 20:59:57 -0400

    Day 01 Haskell part 1 completed

--- 2016/Day_01/Haskell/README.md
+++ 2016/Day_01/Haskell/README.md
@@ -1,5 +1,14 @@
 # What I learned from this day's problem
 
 ## Part 1
-
+SO SO MUCH!!
+- Writing functions in Haskell
+- using head and tail to split an array
+- using fst and snd to get the first and second items in an array or tuple
+- using map in Haskell
+- Using Data.Text to format the data into a usable format
+- putting a bunch of functions back to back to get an answer
+- How to use main
+- how to compile Haskell
+- How to have named inputs to functions and how to use them
 ## Part 2
--- 2016/Day_01/Haskell/solution.hs
+++ 2016/Day_01/Haskell/solution.hs
@@ -18,10 +18,15 @@ splitStringOnCommas x = map T.unpack (map T.strip (T.split (==',') (T.pack x)))
 getComplexDirections :: Num a => String -> [(Complex a, Float)]
 getComplexDirections x = map figureOutDirection (map fixString ((map splitDirDistance (splitStringOnCommas x))))
 
-move :: Num b => (b, b) -> (b, b) -> (b, b)
-move (current_direction, location) (rotate, distance) = (current_direction * rotate, location + (current_direction * rotate * distance))
+--move :: Num b => (b, b) -> (b, b) -> (b, b)
+move (current_direction, location) (rotate, distance) = (current_direction * rotate, location + (current_direction * rotate * (distance:+0)))
 
---finalAnswer j = scanl move (0:+1, 0:+0) (getComplexDirections j)
+finalLocation j = foldl move (0:+1, 0:+0) (getComplexDirections j)
 
---for later
--- abs (realPart (3.0 :+ 2.0)) + abs (imagPart (3.0:+2.0))
+finalAnswer x = abs (realPart (snd x)) + abs (imagPart (snd x))
+
+-- finalAnswer (finalLocation "R2, L3")
+    
+main = do 
+    let puzzleInput = "R4, R4, L1, R3, L5, R2, R5, R1, L4, R3, L5, R2, L3, L4, L3, R1, R5, R1, L3, L1, R3, L1, R2, R2, L2, R5, L3, L4, R4, R4, R2, L4, L1, R5, L1, L4, R4, L1, R1, L2, R5, L2, L3, R2, R1, L194, R2, L4, R49, R1, R3, L5, L4, L1, R4, R2, R1, L5, R3, L5, L4, R4, R4, L2, L3, R78, L5, R4, R191, R4, R3, R1, L2, R1, R3, L1, R3, R4, R2, L2, R1, R4, L5, R2, L2, L4, L2, R1, R2, L3, R5, R2, L3, L3, R3, L1, L1, R5, L4, L4, L2, R5, R1, R4, L3, L5, L4, R5, L4, R5, R4, L3, L2, L5, R4, R3, L3, R1, L5, R5, R1, L3, R2, L5, R5, L3, R1, R4, L5, R4, R2, R3, L4, L5, R3, R4, L5, L5, R4, L4, L4, R1, R5, R3, L1, L4, L3, L4, R1, L5, L1, R2, R2, R4, R4, L5, R4, R1, L1, L1, L3, L5, L2, R4, L3, L5, L4, L1, R3"
+    print (finalAnswer (finalLocation puzzleInput))
