57c37bfc56f5293a7de9d29ce92522a6b231f39b
Eric Mesa <ericsbinaryworld@gmail.com>
2021-10-31 17:43:24 -0400

    2016 Day 03 Ruby Completed

--- 2016/Day_03/Ruby/solution.rb
+++ 2016/Day_03/Ruby/solution.rb
@@ -12,11 +12,22 @@ triangle_triples_input = input_per_line("../input.txt")
 triangle_count_part_1 = 0
 triangle_count_part_2 = 0
 triangle_part_two_array = []
+triangle_2_creator_count = 0
 
 triangle_triples_input.each do |triple|
   this_triple = triple.split(" ")
   triangle_count_part_1 += is_a_triangle(this_triple[0].to_i, this_triple[1].to_i, this_triple[2].to_i)
+  triangle_part_two_array.append(this_triple)
+  triangle_2_creator_count += 1
+  if triangle_2_creator_count % 3 == 0
+    transposed_triples = triangle_part_two_array.transpose
+    transposed_triples.each do |trans_triple|
+      triangle_count_part_2 += is_a_triangle(trans_triple[0].to_i, trans_triple[1].to_i, trans_triple[2].to_i)
+    end
+    triangle_part_two_array.clear
+  end
 end
 
-puts "If we are looking at rows of triplets, there are #{triangle_count_part_1} triangles"
+puts "If we are looking at rows of triples, there are #{triangle_count_part_1} triangles"
 
+puts "If we are looking at columns of triples, there are #{triangle_count_part_2} triangles"
