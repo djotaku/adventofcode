e0e7393c00abd341e4a0ef75906c7864e74edb0a
Eric Mesa <ericsbinaryworld@gmail.com>
2021-11-02 19:02:32 -0400

    Added comments in Haskell code to understand what I did when I come back to this.

--- 2016/Day_01/Haskell/solution.hs
+++ 2016/Day_01/Haskell/solution.hs
@@ -1,28 +1,35 @@
 import Data.Complex
 import qualified Data.Text as T
 
+-- Using the polar math turn one direction if L and the other way if R (or anything, but we know it'll be R)
 figureOutDirection :: Num a => (Char, b) -> (Complex a, b)
 figureOutDirection (dir, distance)
     | dir == 'L' = ((0:+1), distance)
     | otherwise = ((0:+ (-1)), distance)
 
-
+-- This splits the letter from the number in the input
 splitDirDistance :: [a] -> (a, [a])
 splitDirDistance x = (head x, tail x)
 
+-- this converts the number from a string into a Float number
 fixString x = (fst x, read (snd x):: Float)
 
+-- First convert the string into a Data.Text, split on the commas, then turn back into a string.
 splitStringOnCommas :: String -> [String]
 splitStringOnCommas x = map T.unpack (map T.strip (T.split (==',') (T.pack x)))
 
+-- Basically we convert the puzzle input into a series of complex numbers and distances
 getComplexDirections :: Num a => String -> [(Complex a, Float)]
 getComplexDirections x = map figureOutDirection (map fixString ((map splitDirDistance (splitStringOnCommas x))))
 
 --move :: Num b => (b, b) -> (b, b) -> (b, b)
+-- does the actual movement along the blocks one block at a time.
 move (current_direction, location) (rotate, distance) = (current_direction * rotate, location + (current_direction * rotate * (distance:+0)))
 
+-- foldl to apply the move across the entire series of directions
 finalLocation j = foldl move (0:+1, 0:+0) (getComplexDirections j)
 
+-- now that we know where we ended up, take the absolute values of the final coordinates and sum. 
 finalAnswer x = abs (realPart (snd x)) + abs (imagPart (snd x))
 
 -- finalAnswer (finalLocation "R2, L3")
