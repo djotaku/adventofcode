8b6552c95caff83e8e521fd34207b61b33aecfd5
Eric Mesa <ericsbinaryworld@gmail.com>
2021-11-12 18:41:06 -0500

    2016 Day 02 Haskell Compelted
    Part to completed without too much more trouble!

--- 2016/Day_02/Haskell/README.md
+++ 2016/Day_02/Haskell/README.md
@@ -4,3 +4,4 @@
 - Type of quotation matters for thinking of a char or a string. A string has double quotes and a char has single quotes. See [this](https://github.com/djotaku/adventofcode/blob/800175cb4e0a69cb8d15e88d0727a07e7108fd8d/2016/Day_02/Haskell/solution.hs) vs [this](https://github.com/djotaku/adventofcode/blob/61b3be4b88df14ffb06ede25c272be928c06cd40/2016/Day_02/Haskell/solution.hs)
 
 ## Part 2
+- making creative use of othewise to shorten the function to find the next keypad entry
--- 2016/Day_02/Haskell/solution.hs
+++ 2016/Day_02/Haskell/solution.hs
@@ -42,6 +42,44 @@ findNextNumber number direction
     | (number == 9) && (direction == 'R') = 9
 
 
+-- takes in a character for a keypad button and returns the next one based on direction
+findNextKeypadPartTwo :: Char -> Char -> Char
+findNextKeypadPartTwo keypad direction
+    | (keypad == '1') && (direction == 'D') = '3'
+    | (keypad == '2') && (direction == 'D') = '6'
+    | (keypad == '2') && (direction == 'R') = '3'
+    | (keypad == '3') && (direction == 'U') = '1'
+    | (keypad == '3') && (direction == 'D') = '7'
+    | (keypad == '3') && (direction == 'L') = '2'
+    | (keypad == '3') && (direction == 'R') = '4'
+    | (keypad == '4') && (direction == 'D') = '8'
+    | (keypad == '4') && (direction == 'L') = '3'
+    | (keypad == '5') && (direction == 'R') = '6'
+    | (keypad == '6') && (direction == 'U') = '2'
+    | (keypad == '6') && (direction == 'D') = 'A'
+    | (keypad == '6') && (direction == 'L') = '5'
+    | (keypad == '6') && (direction == 'R') = '7'
+    | (keypad == '7') && (direction == 'U') = '3'
+    | (keypad == '7') && (direction == 'D') = 'B'
+    | (keypad == '7') && (direction == 'L') = '6'
+    | (keypad == '7') && (direction == 'R') = '8'
+    | (keypad == '8') && (direction == 'U') = '4'
+    | (keypad == '8') && (direction == 'D') = 'C'
+    | (keypad == '8') && (direction == 'L') = '7'
+    | (keypad == '8') && (direction == 'R') = '9'
+    | (keypad == '9') && (direction == 'L') = '8'
+    | (keypad == 'A') && (direction == 'U') = '6'
+    | (keypad == 'A') && (direction == 'R') = 'B'
+    | (keypad == 'B') && (direction == 'U') = '7'
+    | (keypad == 'B') && (direction == 'D') = 'D'
+    | (keypad == 'B') && (direction == 'L') = 'A'
+    | (keypad == 'B') && (direction == 'R') = 'C'
+    | (keypad == 'C') && (direction == 'U') = '8'
+    | (keypad == 'C') && (direction == 'L') = 'B'
+    | (keypad == 'D') && (direction == 'U') = 'B'
+    | otherwise = keypad
+    
+    
 -- take an array of instructions and a starting number and find the final number
 findNumberRow :: (Foldable t, Eq a, Num a) => a -> t Char -> a
 findNumberRow number directionList = foldl findNextNumber number directionList
@@ -62,7 +100,14 @@ stringFinalAnswer aocinput = concat (map show (finalAnswer aocinput))
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile 
 
+partTwoFindKeyPadRow keypadButton directionList = foldl findNextKeypadPartTwo keypadButton directionList
+
+--partTwoFinalAnswer aocInput = concat (tail (scanl (partTwoFindKeyPadRow 5 aocInput)))
+partTwoFinalAnswer aocInput = tail (scanl partTwoFindKeyPadRow '5' aocInput)
+
 main = do
     ourInput <- readLines "../input.txt"
     print "The answer to part 1 is:"
     print (stringFinalAnswer ourInput)
+    print "The answer to part 2 is:"
+    print (partTwoFinalAnswer ourInput)
