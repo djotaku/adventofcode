61b3be4b88df14ffb06ede25c272be928c06cd40
Eric Mesa <ericsbinaryworld@gmail.com>
2021-11-12 18:11:20 -0500

    Day 02 Part 1 Haskell
    Fixed the characters to actually be characters and now it works!

--- 2016/Day_02/Haskell/README.md
+++ 2016/Day_02/Haskell/README.md
@@ -1,5 +1,6 @@
 # What I learned from this day's problem
 
 ## Part 1
+- Type of quotation matters for thinking of a char or a string. A string has double quotes and a char has single quotes.
 
 ## Part 2
--- 2016/Day_02/Haskell/solution.hs
+++ 2016/Day_02/Haskell/solution.hs
@@ -3,49 +3,48 @@ import qualified Data.Text as T
 
 -- given a number and a direction, give back the new number
 findNextNumber number direction
-    | (number == 1) && (direction == "U") = 1
-    | (number == 1) && (direction == "D") = 4
-    | (number == 1) && (direction == "L") = 1
-    | (number == 1) && (direction == "R") = 2
-    | (number == 2) && (direction == "U") = 2
-    | (number == 2) && (direction == "D") = 5
-    | (number == 2) && (direction == "L") = 1
-    | (number == 2) && (direction == "R") = 3
-    | (number == 3) && (direction == "U") = 3
-    | (number == 3) && (direction == "D") = 6
-    | (number == 3) && (direction == "L") = 2
-    | (number == 3) && (direction == "R") = 3
-    | (number == 4) && (direction == "U") = 1
-    | (number == 4) && (direction == "D") = 7
-    | (number == 4) && (direction == "L") = 4
-    | (number == 4) && (direction == "R") = 5
-    | (number == 5) && (direction == "U") = 2
-    | (number == 5) && (direction == "D") = 8
-    | (number == 5) && (direction == "L") = 4
-    | (number == 5) && (direction == "R") = 6
-    | (number == 6) && (direction == "U") = 3
-    | (number == 6) && (direction == "D") = 9
-    | (number == 6) && (direction == "L") = 5
-    | (number == 6) && (direction == "R") = 6
-    | (number == 7) && (direction == "U") = 4
-    | (number == 7) && (direction == "D") = 7
-    | (number == 7) && (direction == "L") = 7
-    | (number == 7) && (direction == "R") = 8
-    | (number == 8) && (direction == "U") = 5
-    | (number == 8) && (direction == "D") = 8
-    | (number == 8) && (direction == "L") = 7
-    | (number == 8) && (direction == "R") = 9
-    | (number == 9) && (direction == "U") = 6
-    | (number == 9) && (direction == "D") = 9
-    | (number == 9) && (direction == "L") = 8
-    | (number == 9) && (direction == "R") = 9
+    | (number == 1) && (direction == 'U') = 1
+    | (number == 1) && (direction == 'D') = 4
+    | (number == 1) && (direction == 'L') = 1
+    | (number == 1) && (direction == 'R') = 2
+    | (number == 2) && (direction == 'U') = 2
+    | (number == 2) && (direction == 'D') = 5
+    | (number == 2) && (direction == 'L') = 1
+    | (number == 2) && (direction == 'R') = 3
+    | (number == 3) && (direction == 'U') = 3
+    | (number == 3) && (direction == 'D') = 6
+    | (number == 3) && (direction == 'L') = 2
+    | (number == 3) && (direction == 'R') = 3
+    | (number == 4) && (direction == 'U') = 1
+    | (number == 4) && (direction == 'D') = 7
+    | (number == 4) && (direction == 'L') = 4
+    | (number == 4) && (direction == 'R') = 5
+    | (number == 5) && (direction == 'U') = 2
+    | (number == 5) && (direction == 'D') = 8
+    | (number == 5) && (direction == 'L') = 4
+    | (number == 5) && (direction == 'R') = 6
+    | (number == 6) && (direction == 'U') = 3
+    | (number == 6) && (direction == 'D') = 9
+    | (number == 6) && (direction == 'L') = 5
+    | (number == 6) && (direction == 'R') = 6
+    | (number == 7) && (direction == 'U') = 4
+    | (number == 7) && (direction == 'D') = 7
+    | (number == 7) && (direction == 'L') = 7
+    | (number == 7) && (direction == 'R') = 8
+    | (number == 8) && (direction == 'U') = 5
+    | (number == 8) && (direction == 'D') = 8
+    | (number == 8) && (direction == 'L') = 7
+    | (number == 8) && (direction == 'R') = 9
+    | (number == 9) && (direction == 'U') = 6
+    | (number == 9) && (direction == 'D') = 9
+    | (number == 9) && (direction == 'L') = 8
+    | (number == 9) && (direction == 'R') = 9
 
 
 -- take an array of instructions and a starting number and find the final number
 findNumberRow number directionList = foldl findNextNumber number directionList
 
 --puting it all together
-almostFinalAnswer :: (Foldable t, Eq b, Num b) => [t [Char]] -> [b]
 almostFinalAnswer aocinput = scanl findNumberRow 5 aocinput
 
 -- get rid of that extra first number
