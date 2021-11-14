cc3824d64b9b915dedfa0197eb0f1e5599a4469d
Eric Mesa <ericsbinaryworld@gmail.com>
2021-11-13 19:45:10 -0500

    2016 Day 03 Part 1 Haskell Complete

--- 2016/Day_03/Haskell/README.md
+++ 2016/Day_03/Haskell/README.md
@@ -1,5 +1,5 @@
 # What I learned from this day's problem
 
 ## Part 1
-
+- How to do a read to a list so that I can turn a list of numbers into Ints instead of Strings
 ## Part 2
---
+++ 2016/Day_03/Haskell/solution.hs
@@ -0,0 +1,21 @@
+--read from a file and put array where each line is an element. Point free.
+readLines :: FilePath -> IO [String]
+readLines = fmap lines . readFile 
+
+-- Check 3 values to see if they make a valid triangle
+validateTriangle :: (Ord a, Num a, Num p) => [a] -> p
+validateTriangle [side1, side2, side3]
+    | (side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1) = 1
+    | otherwise = 0
+
+-- Take in a string of 3 numbers and run validateTriangle on it
+evaluateTriple :: Num p => String -> p
+evaluateTriple triple = validateTriangle (map (read::String->Int) (words triple))
+
+checkAllPart1Triples :: Num b => [String] -> b
+checkAllPart1Triples triples = foldr (+) 0 (map evaluateTriple triples)
+
+main = do
+    ourInput <- readLines "../input.txt"
+    print "The answer to part 1 is:"
+    print (checkAllPart1Triples ourInput)
