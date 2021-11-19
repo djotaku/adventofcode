33acf7d90db6c2b5082bd29aae2a89d679fe611c
Eric Mesa <ericsbinaryworld@gmail.com>
2021-11-19 18:57:09 -0500

    2016 Day 04 Part 1 Perl Completed
    Thanks to r/adventofcode user allak for point out that I needed to
    inline the multi-sort code

--- 2016/Day_04/Perl/solution.pl
+++ 2016/Day_04/Perl/solution.pl
@@ -4,9 +4,9 @@ use v5.20;
 
 open (ROOMS, "../input.txt") || die "No rooms!";
 
-#my @encrypted_rooms = <ROOMS>;
+my @encrypted_rooms = <ROOMS>;
 # Debug
-my @encrypted_rooms = ("aaaaa-bbb-z-y-x-123[abxyz]", "a-b-c-d-e-f-g-h-987[abcde]", "not-a-real-room-404[oarel]", "totally-real-room-200[decoy]");
+#my @encrypted_rooms = ("aaaaa-bbb-z-y-x-123[abxyz]", "a-b-c-d-e-f-g-h-987[abcde]", "not-a-real-room-404[oarel]", "totally-real-room-200[decoy]");
 my $part_one_answer;
 
 foreach my $encrypted_room (@encrypted_rooms){
@@ -18,20 +18,21 @@ foreach my $encrypted_room (@encrypted_rooms){
     foreach my $character (split //, $room){
         $letter_count{$character}++;
     }
-    say %letter_count;
-    my @letters = sort numerical_then_alphabetical keys %letter_count;
+    my @letters = sort {$letter_count{$b} <=> $letter_count{$a}
+        or
+        $a cmp $b
+    } keys %letter_count;
     
     sub numerical_then_alphabetical{
         $letter_count{$b} <=> $letter_count{$a}
         or
         $a cmp $b
     }
-    say @letters;
     my @checksum_letters = (split //, $checksum);
     my $checksum_letters_len = @checksum_letters;
     my $truth_count = 0;
     foreach my $number (0..($checksum_letters_len-1)){
-        if ($letters[$number] == $checksum_letters[$number]){
+        if ($letters[$number] eq $checksum_letters[$number]){
             $truth_count++;
         }
     }
