96b3e2d4b3a5f923e98e8ae92d17b029807cac9a
Eric Mesa <ericsbinaryworld@gmail.com>
2021-11-18 20:47:27 -0500

    2016 Day 04 Part 1 Perl
    My multi-sort isn't quite working correctly. At least not on the last
    two examples.

--- 2016/Day_04/Perl/README.md
+++ 2016/Day_04/Perl/README.md
@@ -1,5 +1,6 @@
 # What I learned from this day's problem
 
 ## Part 1
-
+- Reminder of the perl join function and its syntax
+- Multi-level sorting in Perl
 ## Part 2
--- 2016/Day_04/Perl/solution.pl
+++ 2016/Day_04/Perl/solution.pl
@@ -4,13 +4,40 @@ use v5.20;
 
 open (ROOMS, "../input.txt") || die "No rooms!";
 
-my @encrypted_rooms = <ROOMS>;
+#my @encrypted_rooms = <ROOMS>;
+# Debug
+my @encrypted_rooms = ("aaaaa-bbb-z-y-x-123[abxyz]", "a-b-c-d-e-f-g-h-987[abcde]", "not-a-real-room-404[oarel]", "totally-real-room-200[decoy]");
+my $part_one_answer;
 
 foreach my $encrypted_room (@encrypted_rooms){
 
+    my %letter_count;
     my @my_room = $encrypted_room =~ /(\w+)-/g;
-    my @sector_and_checksum = $encrypted_room =~ /(\d+)\[(\w+)\]/g;
+    my $room = join("", @my_room);
+    my ($sector, $checksum) = $encrypted_room =~ /(\d+)\[(\w+)\]/g;
+    foreach my $character (split //, $room){
+        $letter_count{$character}++;
+    }
+    say %letter_count;
+    my @letters = sort numerical_then_alphabetical keys %letter_count;
     
-    say "@my_room";
-    say "$my_room[0]";
+    sub numerical_then_alphabetical{
+        $letter_count{$b} <=> $letter_count{$a}
+        or
+        $a cmp $b
+    }
+    say @letters;
+    my @checksum_letters = (split //, $checksum);
+    my $checksum_letters_len = @checksum_letters;
+    my $truth_count = 0;
+    foreach my $number (0..($checksum_letters_len-1)){
+        if ($letters[$number] == $checksum_letters[$number]){
+            $truth_count++;
+        }
+    }
+    if ($truth_count == 5){
+        $part_one_answer += $sector;
+    }
 }
+
+say "The sum of valid sector IDs is $part_one_answer.";
