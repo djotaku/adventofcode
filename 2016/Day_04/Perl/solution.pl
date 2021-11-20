d87adc7adefe43b7ed2033a06deaf048ebbc026e
Eric Mesa <ericsbinaryworld@gmail.com>
2021-11-19 19:27:55 -0500

    2016 Day 04 Part 2 Perl
    working through it. Not sure why index function isn't working

--- 2016/Day_04/Perl/solution.pl
+++ 2016/Day_04/Perl/solution.pl
@@ -8,6 +8,7 @@ my @encrypted_rooms = <ROOMS>;
 # Debug
 #my @encrypted_rooms = ("aaaaa-bbb-z-y-x-123[abxyz]", "a-b-c-d-e-f-g-h-987[abcde]", "not-a-real-room-404[oarel]", "totally-real-room-200[decoy]");
 my $part_one_answer;
+my $part_two_answer;
 
 foreach my $encrypted_room (@encrypted_rooms){
 
@@ -23,11 +24,6 @@ foreach my $encrypted_room (@encrypted_rooms){
         $a cmp $b
     } keys %letter_count;
     
-    sub numerical_then_alphabetical{
-        $letter_count{$b} <=> $letter_count{$a}
-        or
-        $a cmp $b
-    }
     my @checksum_letters = (split //, $checksum);
     my $checksum_letters_len = @checksum_letters;
     my $truth_count = 0;
@@ -38,7 +34,26 @@ foreach my $encrypted_room (@encrypted_rooms){
     }
     if ($truth_count == 5){
         $part_one_answer += $sector;
+        my $cipher_increment = $sector % 26;
+        my $decrypted_room;
+        foreach my $character (split //, $room){
+            foreach my $num (1..$cipher_increment){
+                if ($character eq "z"){
+                    $character = "a";
+                }else{
+                    $character++;
+                }
+                #say "character in loop = $character";
+            }
+            #say "-----------------";
+            $decrypted_room .= $character;
+        }
+        #say "$decrypted_room";
+        if (index($decrypted_room, "object" != -1)){
+            $part_two_answer = $sector;
+        }
     }
 }
 
 say "The sum of valid sector IDs is $part_one_answer.";
+say "The correct room is in sector $part_two_answer.";
