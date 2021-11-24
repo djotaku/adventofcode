021104fec5a5e8c14f66414d8881169ec5a1cf45
Eric Mesa <ericsbinaryworld@gmail.com>
2021-11-23 21:22:42 -0500

    2016 Day 05 Part 2 Ruby completed
    As a bonus, the second door password fills in like in WarGames

--- 2016/Day_05/Ruby/solution.rb
+++ 2016/Day_05/Ruby/solution.rb
@@ -26,8 +26,35 @@ def find_number_part_one(secret_key)
     door_one
 end
 
+def find_number_part_two(secret_key)
+    number = 1
+    password_hash = secret_key + number.to_s
+    door_two = ["", "", "", "", "", "", "", ""]
+    while door_two.include? ""
+        until start_with_five_zeroes(create_hash(password_hash))
+            number += 1
+            password_hash = secret_key + number.to_s
+        end
+        this_hash = create_hash(password_hash)
+        # puts this_hash
+        if this_hash[5].match(/\d/)
+            if this_hash[5].to_i < 8
+                if door_two[this_hash[5].to_i] == ""
+                    door_two[this_hash[5].to_i] = this_hash[6]
+                end
+            end
+        end
+        number += 1
+        password_hash = secret_key + number.to_s
+        puts "#{door_two}"
+    end
+    door_two
+end
+
 if $PROGRAM_NAME == __FILE__
   input = "ugkcyxxp"
   door_one = find_number_part_one(input)
-  puts "The password to the first door is: #{door_one}"
+  puts "The password to the first door is: #{door_one}.\n"
+  door_two = find_number_part_two(input)
+  puts "The answer to the second door is #{door_two}."
 end
