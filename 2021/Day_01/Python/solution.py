180ed51255acab2215cd7e2e49a0ecd93265b06b
Eric Mesa <ericsbinaryworld@gmail.com>
2021-12-01 06:25:20 -0500

    2021 Day 01 Part 2 Python
    Refactor in prep for part 2

--- 2021/Day_01/Python/solution.py
+++ 2021/Day_01/Python/solution.py
@@ -7,15 +7,20 @@ def input_per_line(file: str):
         return [line.rstrip() for line in input_file.readlines()]
 
 
-if __name__ == "__main__":
-    sonar_measurements = input_per_line("../input.txt")
+def part_one_sonar(part_one_sonar_measurements):
     increase_count = 0
-    previous_measurement = int(sonar_measurements[0])
-    for index in range(1, len(sonar_measurements)):
-        current_measurement = int(sonar_measurements[index])
+    previous_measurement = int(part_one_sonar_measurements[0])
+    for index in range(1, len(part_one_sonar_measurements)):
+        current_measurement = int(part_one_sonar_measurements[index])
         if current_measurement > previous_measurement:
             increase_count += 1
         previous_measurement = current_measurement
-    print(f"There are {increase_count} measurements larger than the previous one.")
+    return increase_count
+
+
+if __name__ == "__main__":
+    sonar_measurements = input_per_line("../input.txt")
+    part_one_answer = part_one_sonar(sonar_measurements)
+    print(f"There are {part_one_answer} measurements larger than the previous one.")
 
 
