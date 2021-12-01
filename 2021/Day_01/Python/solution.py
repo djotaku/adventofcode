9dfeb5824c02f58308db2d7da5cc48916ed7e7f3
Eric Mesa <ericsbinaryworld@gmail.com>
2021-12-01 06:32:29 -0500

    2021 Day 01 Part 2 Completed Python
    Easy day 1

--- 2021/Day_01/Python/solution.py
+++ 2021/Day_01/Python/solution.py
@@ -18,9 +18,24 @@ def part_one_sonar(part_one_sonar_measurements):
     return increase_count
 
 
+def part_two_sonar(part_two_sonar_measurements):
+    increase_count = 0
+    previous_triple = int(part_two_sonar_measurements[0]) + int(part_two_sonar_measurements[1]) + \
+                      int(part_two_sonar_measurements[2])
+    for index in range(1, len(part_two_sonar_measurements)-2):
+        current_triple = int(part_two_sonar_measurements[index]) + int(part_two_sonar_measurements[index+1]) + \
+                      int(part_two_sonar_measurements[index+2])
+        if current_triple > previous_triple:
+            increase_count += 1
+        previous_triple = current_triple
+    return increase_count
+
+
 if __name__ == "__main__":
     sonar_measurements = input_per_line("../input.txt")
     part_one_answer = part_one_sonar(sonar_measurements)
+    part_two_answer = part_two_sonar(sonar_measurements)
     print(f"There are {part_one_answer} measurements larger than the previous one.")
+    print(f"If measuring by triples, {part_two_answer} measurements are larger than the previous one.")
 
 
