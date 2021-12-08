1c9cbc00685ad705d08e10a6df95bf2a6361f741
Eric Mesa <ericsbinaryworld@gmail.com>
2021-12-08 12:42:20 -0500

    2021 Day 08 Part 2 Python
    Working on it. scaffolding

--- 2021/Day_08/Python/solution.py
+++ 2021/Day_08/Python/solution.py
@@ -14,6 +14,29 @@ FOUR = 4
 SEVEN = 3
 EIGHT = 7
 
+
+# 7 segs  8
+# 6 segs  0, 6, 9
+#             missing segment in 6 is a segment in 1 or 7
+#             missing segment in 0 is a segment in 4 but not 1
+#            missing segment in 9 is not a segment in 1,4,or 7
+# 5 segs  2, 3, 5
+#             both missing segments in 2 are in 4
+#             only one missing segment in 5 is in 4
+#             both missing segments in 3 are not in 1 or 7
+# 4 segs  4
+# 3 segs  7
+# 2 segs  1
+
+
+def decode_segments(scrambled_line: str) -> dict:
+    """Take in string of scrambled digits.
+
+    Return a decoding dictionary to analyze the output.
+    """
+
+
+
 if __name__ == "__main__":
     scrambled_input = input_per_line("../input.txt")
     digit_count = 0
