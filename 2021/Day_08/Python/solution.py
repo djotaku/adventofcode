01f0d2ea3900cc463ab3141d017fa3daa3deabaf
Eric Mesa <ericsbinaryworld@gmail.com>
2021-12-08 12:59:05 -0500

    2021 Day 08 Part 2 Python
    Working on it. Even more scaffolding

--- 2021/Day_08/Python/solution.py
+++ 2021/Day_08/Python/solution.py
@@ -13,13 +13,15 @@ ONE = 2
 FOUR = 4
 SEVEN = 3
 EIGHT = 7
+ZERO_SIX_NINE = 6
+TWO_THREE_FIVE = 5
 
 
 # 7 segs  8
 # 6 segs  0, 6, 9
 #             missing segment in 6 is a segment in 1 or 7
 #             missing segment in 0 is a segment in 4 but not 1
-#            missing segment in 9 is not a segment in 1,4,or 7
+#             missing segment in 9 is not a segment in 1,4,or 7
 # 5 segs  2, 3, 5
 #             both missing segments in 2 are in 4
 #             only one missing segment in 5 is in 4
@@ -34,6 +36,30 @@ def decode_segments(scrambled_line: str) -> dict:
 
     Return a decoding dictionary to analyze the output.
     """
+    decoded_dictionary = {}
+    scrambled_digits = scrambled_line.split()
+    this_one = ""
+    this_four = ""
+    this_seven = ""
+    this_eight = ""
+    for this_digit in scrambled_digits:
+        if len(this_digit) == ONE:
+            decoded_dictionary[this_digit] = 1
+            this_one = this_digit
+        elif len(this_digit) == FOUR:
+            decoded_dictionary[this_digit] = 4
+            this_four = = this_digit
+        elif len(this_digit) == SEVEN:
+            decoded_dictionary[this_digit] = 7
+            this_seven = = this_digit
+        elif len(this_digit) == EIGHT:
+            decoded_dictionary[this_digit] = 8
+            this_eight= this_digit
+        elif len(this_digit) == ZERO_SIX_NINE:
+            this_digit_letters = [letter for letter in this_digit]
+            # let's see if 6 first
+            letters_in_common_with_one = 0
+
 
 
 
