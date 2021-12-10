197544f90fe8bc84032869349dc325d8cdc17ff3
Eric Mesa <ericsbinaryworld@gmail.com>
2021-12-09 19:29:58 -0500

    2021 Day 09 Part 2 Python
    Works for top left example. Time to try top-right

--- 2021/Day_09/Python/solution.py
+++ 2021/Day_09/Python/solution.py
@@ -70,36 +70,84 @@ def find_low_points_part_2(heightmap: dict, max_x: int, max_y: int) -> list:
     return low_points_coordinates
 
 
-def find_basin_members(minimum_coordinate: tuple, heightmap: dict, max_x: int, max_y: int) -> list:
-    """Take in the coordinate for a minimum and then search for members of the basin.
-    We stop when we get surrounded by 9s.
+def find_basin_members(current_coordinate: tuple, heightmap: dict, do_not_search: set) -> (list, set):
+    """Take in a coordinate, heightmap, and set of coordinates that should not be searched.
 
-    Return a list of the numbers.
+    Generate a new list of coordinates to search and add to the coordinates not to search.
+
+    If completely surrounded by do_not_search coordinates, return that coordinate and add it to
+    do_not_search.
+
+    Return a list of the coordinates and sets.
     """
-    initial_x = minimum_coordinate[0]
-    initial_y = minimum_coordinate[1]
-    basin_members = [heightmap[minimum_coordinate]]
-    # search left
-    for x in range(initial_x - 1, -1, -1):
-        if heightmap[(x, initial_y)] == 9:
-            break
-        else:
-            basin_members.append(heightmap[(x, initial_y)])
-    # search right
-    for x in range(initial_x + 1, max_x):
-        if heightmap[(x, initial_y)] == 9:
-            break
-        else:
-            basin_members.append(heightmap[(x, initial_y)])
-    return basin_members
-
+    print(f"{current_coordinate=}")
+    print(f"{heightmap.get(current_coordinate)=}")
+    print(f"{do_not_search=}")
+    return_list = []
+    initial_x = current_coordinate[0]
+    initial_y = current_coordinate[1]
+    left_coordinate = (initial_x - 1, initial_y)
+    right_coordinate = (initial_x + 1, initial_y)
+    above_coordinate = (initial_x, initial_y - 1)
+    below_coordinate = (initial_x, initial_y + 1)
+    do_not_search.add(current_coordinate)
+    # current coordinate is surrounded coordinates already in basin tally
+    if left_coordinate in do_not_search and right_coordinate in do_not_search and above_coordinate in do_not_search and below_coordinate in do_not_search:
+        print("I think I'm here?")
+        do_not_search.add(current_coordinate)
+        return [current_coordinate], do_not_search
+    # current coordinate is 9 - may be necessary based on what I'm doing below
+    if heightmap.get(current_coordinate) == 9:
+        print("I'm a 9?")
+        do_not_search.add(current_coordinate)
+        return [], do_not_search
+    left = heightmap.get(left_coordinate, 99999)
+    if left in [99999, 9]:
+        do_not_search.add(left_coordinate)
+    right = heightmap.get(right_coordinate, 99999)
+    if right in [99999, 9]:
+        do_not_search.add(right_coordinate)
+    above = heightmap.get(above_coordinate, 99999)
+    if above in [99999, 9]:
+        do_not_search.add(above_coordinate)
+    below = heightmap.get(below_coordinate, 99999)
+    if below in [99999, 9]:
+        do_not_search.add(below_coordinate)
+    if left_coordinate not in do_not_search:
+        print("I'm in left")
+        left_return_list, left_do_not_search = find_basin_members(left_coordinate, heightmap, do_not_search)
+        print(f"{left_do_not_search=}")
+        print(f"{left_return_list=}")
+        return_list += left_return_list
+        do_not_search = set.union(do_not_search, left_do_not_search)
+    if right_coordinate not in do_not_search:
+        print("I'm right")
+        right_return_list, right_do_not_search = find_basin_members(right_coordinate, heightmap, do_not_search)
+        return_list += right_return_list
+        do_not_search = set.union(do_not_search, right_do_not_search)
+    if above_coordinate not in do_not_search:
+        print("I'm above")
+        above_return_list, above_do_not_search = find_basin_members(above_coordinate, heightmap, do_not_search)
+        return_list += above_return_list
+        do_not_search = set.union(do_not_search, above_do_not_search)
+    if below_coordinate not in do_not_search:
+        print("I'm above")
+        below_return_list, below_do_not_search = find_basin_members(below_coordinate, heightmap, do_not_search)
+        return_list += below_return_list
+        do_not_search = set.union(do_not_search, below_do_not_search)
+    return_list.append(heightmap.get(current_coordinate))
+    print(f"{return_list=}")
+    return return_list, do_not_search
 
 
 if __name__ == "__main__":
-    our_heights = input_per_line("../input.txt")
+    #our_heights = input_per_line("../input.txt")
+    our_heights = input_per_line("../test_input.txt")
     this_heightmap, this_x, this_y = create_basin_heightmap(our_heights)
     our_low_points = find_low_points(this_heightmap, this_x, this_y)
     answer = sum(our_low_points) + len(our_low_points)
     print(f"The sum of the low points is {answer}")
+    empty_set = set()
+    answer_two, no_search_two = find_basin_members((1, 0), this_heightmap, empty_set)
 
 # 346 is too low
