6866f956bbc321a400fb3602810be9c526d7297c
Eric Mesa <ericsbinaryworld@gmail.com>
2021-12-19 20:39:57 -0500

    2021 Day 15 Part 1 Python
    Kinda, sorta where I need to be. Almost.

--- 2021/Day_15/Python/solution.py
+++ 2021/Day_15/Python/solution.py
@@ -49,36 +49,19 @@ def create_adjacency_grid(length_of_grid: int) -> dict:
     return this_adjacency_grid
 
 
-def traverse_graph(visited, graph, bottom_right: tuple, can_revisit=True):
+def traverse_graph(visited, graph, bottom_right: tuple, can_revisit, grid_dictionary):
     """Do a depth-first search of the graph.
-
-    Only visit small nodes once (or allow one of them to go twice in part 2).
-
-    Stop bottom_right - bottom right tuple
-
-    if next_node not in visited or next_node.isupper() or can_revisit
-
-    it will over-ride being able to not visit a lowercase letter twice. But
-    after that it will be in visited and when you get to the line:
-
-    can_revisit and not (next_node.islower() and next_node in visited)
-
-    it'll be can_revisit=False going forward.
     """
     # check if we've reached bottom_right to end the recursion
     # print(f"{visited=}")
     if visited[-1] == bottom_right:
         # print("I am at end")
-        return visited
-    return [
-        traverse_graph(
-            visited + [next_node],
-            graph, bottom_right,
-            False
-        )
-        for next_node in graph[visited[-1]]
-        if next_node not in visited or can_revisit
-        ]
+        return sum(int(grid_dictionary[point]) for point in visited if point != (0, 0))
+
+    return [traverse_graph(visited + [next_node], graph, bottom_right, False, grid_dictionary)
+            for next_node in graph[visited[-1]]
+            if next_node not in visited or can_revisit
+            ]
 
 
 if __name__ == "__main__":
@@ -86,9 +69,7 @@ if __name__ == "__main__":
     grid_of_points = create_grid(points)
     length_of_grid = len(points)
     adjacency_grid = create_adjacency_grid(length_of_grid)
-    # pprint(adjacency_grid)
+    pprint(adjacency_grid)
     # print(f"{length_of_grid-1}")
-    find_paths = traverse_graph([(0, 0)], adjacency_grid, (length_of_grid-1, length_of_grid-1), False)
-    # pprint(f"{find_paths=}")
-    for path in find_paths:
-        print(f"{path=}")
+    find_paths = traverse_graph([(0, 0)], adjacency_grid, (length_of_grid-1, length_of_grid-1), False, grid_of_points)
+    pprint(f"{find_paths=}")
