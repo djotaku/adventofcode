8fa83aad378b52ccc6b14b65eb431efafb474fed
Eric Mesa <ericsbinaryworld@gmail.com>
2021-12-21 23:58:25 -0500

    2021 Day 21 Part 1 Python
    Almost there, I think

--- 2021/Day_21/Python/solution.py
+++ 2021/Day_21/Python/solution.py
@@ -28,6 +28,9 @@ class Player:
         self.move_player(die_roll)
         self.update_score()
 
+    def __repr__(self):
+        return f"{self.player_name} at {self.location} with score: {self.player_score}"
+
 
 class Die:
     def __init__(self):
@@ -39,16 +42,54 @@ class Die:
         if practice_mode:
             self.number_rolled += 1
             self.times_rolled += 1
+        return self.number_rolled
+
+    def __repr__(self):
+        return f"Die rolled {self.times_rolled} times. Most recent number rolled {self.number_rolled}"
 
 
 if __name__ == "__main__":
-    player_starting_positions = input_per_line("../input.txt")
+    player_starting_positions = input_per_line("../test_input.txt")
     # create the players
+    _, player_one_starting_position = player_starting_positions[0].split(":")
+    player_one_starting_position = int(player_one_starting_position)
+    _, player_two_starting_position = player_starting_positions[1].split(":")
+    player_two_starting_position = int(player_two_starting_position)
+    player_one = Player("Player 1", player_one_starting_position)
+    player_two = Player("Player 2", player_two_starting_position)
     max_player_score = 0
+    # create die
+    part_one_die = Die()
     # while loop that ends when max_player_score > 999
-    # roll die 3 times
-    # player one
-    # roll die 3 times
-    # player 2
-    # max_player_score = max(player_one.player_score, player_two.player_score)
-    # do the calculation for the puzzle submition
+    print("About to start game")
+    print(player_one)
+    print(player_two)
+    while max_player_score < 1000:
+        print("-------")
+        print("New Round")
+        # roll die 3 times
+        rolls = [part_one_die.next_roll(practice_mode=True), part_one_die.next_roll(practice_mode=True),
+                 part_one_die.next_roll(practice_mode=True)]
+        player_one_moves = sum(rolls)
+        print(f"{player_one_moves=}")
+        player_one.complete_move(player_one_moves)
+        rolls.clear()
+        rolls = [part_one_die.next_roll(practice_mode=True), part_one_die.next_roll(practice_mode=True),
+                 part_one_die.next_roll(practice_mode=True)]
+        player_two_moves = sum(rolls)
+        player_two.complete_move(player_two_moves)
+        rolls.clear()
+        print(player_one)
+        print(player_two)
+        max_player_score = max(player_one.player_score, player_two.player_score)
+    # someone has scored 1000
+    print("Game Over.")
+    print(player_one)
+    print(player_two)
+    print(part_one_die)
+    loser_score = min(player_one.player_score, player_two.player_score)
+    die_rolls = part_one_die.times_rolled
+    print(f"Loser score times number of die rolls is {loser_score * die_rolls}")
+
+
+# 1080000 is too high
