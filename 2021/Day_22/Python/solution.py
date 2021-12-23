5351e20d974acca6beb3f2dab472283f943a7653
Eric Mesa <ericsbinaryworld@gmail.com>
2021-12-23 14:03:59 -0500

    2021 Day 22 Part 1 Python
    Sets don't work either

--- 2021/Day_22/Python/solution.py
+++ 2021/Day_22/Python/solution.py
@@ -37,6 +37,23 @@ def initialize_cuboid(instruction, current_cubes: dict) -> dict:
     return current_cubes
 
 
+def initialize_cuboid_sets(instruction, current_cubes: dict) -> dict:
+    """Take in instruction tuple and return dictionary with the modified cores."""
+    for x in range(instruction.x_min, instruction.x_max+1):
+        for y in range(instruction.y_min, instruction.y_max+1):
+            for z in range(instruction.z_min, instruction.z_max+1):
+                if instruction.on_or_off == "on":
+                    current_cubes[(x, y, z)] = 1
+                    # current_cubes.add((x, y, z))
+                elif instruction.on_or_off == "off":
+                    current_cubes[(x, y, z)] = 0
+                    try:
+                        # current_cubes.remove((x, y, z))
+                    except KeyError:
+                        pass
+    return current_cubes
+
+
 def cubes_in_part_one(cubes_at_end: dict) -> int:
     """Sum up the turned on cubes in -50-50 cuboid."""
     return sum(cubes_at_end.get((x, y, z), 0) for x in range(-50, 50)
@@ -46,7 +63,7 @@ def cubes_in_part_one(cubes_at_end: dict) -> int:
 
 if __name__ == "__main__":
     our_instructions = input_per_line("../larger_sample_input.txt")
-    cubes = {}
+    cubes = set()
     for the_instruction in our_instructions:
         this_instruction = parse_instruction(the_instruction)
         cubes = initialize_cuboid(this_instruction, cubes)
