cfbf9c3f08ccb5d1bb12b1e6036b65deeac947f6
Eric Mesa <ericsbinaryworld@gmail.com>
2022-02-05 22:04:58 -0500

    2016 Day 5 Golang completed!

--- 2016/Day_05/Go/solution.go
+++ 2016/Day_05/Go/solution.go
@@ -47,7 +47,34 @@ func createDoorOnePassword(aocInput string) string {
 	return password
 }
 
+// createDoorTwoPassword takes in the aoc input and uses the new rules to produce the door password
+func createDoorTwoPassword(aocInput string) string {
+	var password [8]rune
+	var locationCheck [8]bool
+	numericalSuffix := 0
+	for locationCheck[0] == false || locationCheck[1] == false || locationCheck[2] == false || locationCheck[3] == false || locationCheck[4] == false || locationCheck[5] == false || locationCheck[6] == false || locationCheck[7] == false {
+		testHash := createHash(aocInput + strconv.Itoa(numericalSuffix))
+		if validateHash(testHash) {
+			probableLocation := string([]rune(testHash)[5])
+			if probableLocation == "0" || probableLocation == "1" || probableLocation == "2" || probableLocation == "3" || probableLocation == "4" || probableLocation == "5" || probableLocation == "6" || probableLocation == "7" {
+				location, _ := strconv.Atoi(probableLocation)
+				character := []rune(testHash)[6]
+				if locationCheck[location] == false {
+					locationCheck[location] = true
+					password[location] = character
+				}
+
+			}
+		}
+		numericalSuffix++
+	}
+	return string(password[:])
+}
+
 func main() {
-	doorOnePassword := createDoorOnePassword("ugkcyxxp")
-	fmt.Printf("The password to door #1 is %s", doorOnePassword)
+	aocInput := "ugkcyxxp"
+	doorOnePassword := createDoorOnePassword(aocInput)
+	fmt.Printf("The password to door #1 is %s\n", doorOnePassword)
+	doorTwoPassword := createDoorTwoPassword(aocInput)
+	fmt.Printf("The password to door #2 is %s", doorTwoPassword)
 }
