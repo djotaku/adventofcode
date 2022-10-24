a1713908d6d696111181d4abb9ea29883b8c33e4
Eric Mesa <ericsbinaryworld@gmail.com>
2022-10-24 11:29:21 -0400

    2016 Day 15 Part 1 Python
    got the data extraction function done

--- 2016/Day_15/Python/solution.py
+++ 2016/Day_15/Python/solution.py
@@ -1,5 +1,5 @@
 """Solution for AoC 2016 Day 15: Timing is Everything"""
-import math
+import re
 
 
 def find_capsule_time(discs: list) -> int:
@@ -10,7 +10,6 @@ def find_capsule_time(discs: list) -> int:
     Order matters, it should be the order of discs in the machine.
     """
     # reaches the first disc at t=1.
-    # first figure out the time points where each disc gets to 0 the first time
     time = 0
     loop = True
     drop_check = []
@@ -25,6 +24,24 @@ def find_capsule_time(discs: list) -> int:
             loop = False
         drop_check.clear()
     return time
+
+
+def extract_data(puzzle_input: list) -> list:
+    """Take in a list where each item looks like:
+
+    Disc #1 has 5 positions: at time=0, it is at position 4.
+
+    and returns a list with items like (based on above):
+
+    (5, 4)
+    """
+    regex = re.compile(r'(\d+) positions: at time=0, it is at position (\d+)\.')
+    output_list = []
+    for sentence in puzzle_input:
+        numbers = re.findall(regex, sentence)
+        print(numbers)
+        output_list.append((int(numbers[0][0]), int(numbers[0][1])))
+    return output_list
 
 
 if __name__ == "__main__":
--- 2016/Day_15/Python/test_solution.py
+++ 2016/Day_15/Python/test_solution.py
@@ -4,3 +4,12 @@ import solution
 def test_find_capsule_time():
     discs = [(5, 4), (2, 1)]
     assert solution.find_capsule_time(discs) == 5
+
+
+def test_extra_data():
+    test_sentences = [
+        "Disc #1 has 5 positions: at time=0, it is at position 4.",
+        "Disc #1 has 2 positions: at time=0, it is at position 1."
+    ]
+    extracted_data = solution.extract_data(test_sentences)
+    assert extracted_data == [(5, 4), (2, 1)]
