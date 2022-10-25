23940bf9b50957e5a888e29e8f0a5e1e5b97ed0e
Eric Mesa <ericsbinaryworld@gmail.com>
2022-10-24 21:24:01 -0400

    2016 Day 16 Part 1 Python
    Almost done

--- 2016/Day_16/Python/solution.py
+++ 2016/Day_16/Python/solution.py
@@ -10,3 +10,32 @@ def flip_bits(bit_string: str) -> str:
         else:
             flipped.append('0')
     return ''.join(flipped)
+
+
+def generate_checksum(data_to_check: str) -> str:
+    """Take in a bit string and generate the checksum.
+
+    If the length of the checksum is even, run the process again.
+    """
+    checksum = []
+    for index in range(0, len(data_to_check) - 1, 2):
+        if data_to_check[index] == data_to_check[index + 1]:
+            checksum.append('1')
+        else:
+            checksum.append('0')
+    if len(checksum) % 2 == 0:
+        return generate_checksum(''.join(checksum))
+    else:
+        return ''.join(checksum)
+
+
+def fill_disk(initial_state: str, fill_length: int) -> str:
+    """Build up a string using the problem's rules, up to the fill_length."""
+    length_of_string = len(initial_state)
+    our_string = initial_state
+    while length_of_string < fill_length:
+        our_string_reverse = "".join(reversed(our_string))
+        suffix = flip_bits(our_string_reverse)
+        our_string = our_string + "0" + suffix
+        length_of_string = len(our_string)
+    return our_string[0:fill_length]
--- 2016/Day_16/Python/test_solution.py
+++ 2016/Day_16/Python/test_solution.py
@@ -5,3 +5,11 @@ def test_flip_bits():
     assert solution.flip_bits('1') == '0'
     assert solution.flip_bits('00') == "11"
     assert solution.flip_bits('01') == '10'
+
+
+def test_generate_checksum():
+    assert solution.generate_checksum('110010110100') == "100"
+
+
+def test_fill_disk():
+    assert solution.fill_disk("10000", 20) == "10000011110010000111"
