debe4269048bff7612a3a16b574cf3b0a54cde42
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-04 15:18:48 -0500

    2022 Day 04 Ruby Part 1 solution

--- 2022/Day_04/Ruby/solution.rb
+++ 2022/Day_04/Ruby/solution.rb
@@ -1,5 +1,24 @@
 # Solution for AoC 2022 Day 04 - Camp Cleanup
 
+require 'set'
+
 def input_per_line(file)
   File.readlines(file, chomp:true)
 end
+
+
+elf_pairs = input_per_line("../input.txt")
+part_1_sum = 0
+elf_pairs.each do |line|
+  (left_elf, right_elf) = line.split(",") 
+  (left_elf_start, left_elf_end) = left_elf.split("-")
+  (right_elf_start, right_elf_end) = right_elf.split("-")
+  left_elf_sections = (left_elf_start..left_elf_end).to_a.to_set
+  right_elf_sections = (right_elf_start..right_elf_end).to_a.to_set
+  if left_elf_sections.subset?(right_elf_sections) or right_elf_sections.subset?(left_elf_sections)
+    part_1_sum += 1
+  end
+end
+
+puts "The elves who have full subsets of the other total: #{part_1_sum}."
+
