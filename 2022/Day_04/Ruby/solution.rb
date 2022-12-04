32be61452fd38995c6807a1fe5350190b6bea208
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-04 15:22:57 -0500

    2022 Day 04 Ruby completed

--- 2022/Day_04/Ruby/solution.rb
+++ 2022/Day_04/Ruby/solution.rb
@@ -9,6 +9,7 @@ end
 
 elf_pairs = input_per_line("../input.txt")
 part_1_sum = 0
+part_2_sum = 0
 elf_pairs.each do |line|
   (left_elf, right_elf) = line.split(",") 
   (left_elf_start, left_elf_end) = left_elf.split("-")
@@ -18,7 +19,11 @@ elf_pairs.each do |line|
   if left_elf_sections.subset?(right_elf_sections) or right_elf_sections.subset?(left_elf_sections)
     part_1_sum += 1
   end
+  if not left_elf_sections.disjoint?(right_elf_sections) or not right_elf_sections.disjoint?(left_elf_sections)
+    part_2_sum += 1
+  end
+
 end
 
 puts "The elves who have full subsets of the other total: #{part_1_sum}."
-
+puts "The elves who have at least one section in common total: #{part_2_sum}."
