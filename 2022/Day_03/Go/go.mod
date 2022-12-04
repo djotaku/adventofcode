4fbc58e82fbbe3dc05ee8f7e258d81b6319581ad
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-04 14:59:00 -0500

    2022 Day 04 Ruby starting

---
+++ 2022/Day_03/Go/go.mod
@@ -0,0 +1,5 @@
+module github.com/djotaku/aoc_2022_day_03
+
+go 1.19
+
+require github.com/deckarep/golang-set v1.8.0 // indirect
---
+++ 2022/Day_04/Ruby/solution.rb
@@ -0,0 +1,5 @@
+# Solution for AoC 2022 Day 04 - Camp Cleanup
+
+def input_per_line(file)
+  File.readlines(file, chomp:true)
+end
