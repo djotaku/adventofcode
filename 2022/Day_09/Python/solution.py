609472e9daa17e1977d9400b0961a84ae749d061
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-09 17:14:50 -0500

    2022 Day 09 Python Part 1
    Off by one. I think I need to include the original spot?

--- 2022/Day_09/Python/solution.py
+++ 2022/Day_09/Python/solution.py
@@ -24,48 +24,63 @@ def simulation(moves: list) -> int:
         direction = move[0]
         distance = int(move[1])
         # debug
-
+        print("**************")
+        print("start of moves")
+        print(f"{direction} {distance}")
+        print(f"{head_pos=}")
+        print(f"{tail_pos=}")
         # end debug
         if direction == "U":
-            for y in range(distance + 1):
+            for y in range(distance):
                 head_pos[1] += 1
                 if should_tail_move(head_pos, tail_pos):
                     if should_tail_move_straight(head_pos, tail_pos):
                         tail_pos[1] += 1
+                        positions_visited.add((tail_pos[0], tail_pos[1]))
                     else:  # diag
                         tail_pos[0] = head_pos[0]
                         tail_pos[1] += 1
                         positions_visited.add((tail_pos[0], tail_pos[1]))
         elif direction == "D":
-            for y in range(distance + 1):
+            for y in range(distance):
                 head_pos[1] -= 1
                 if should_tail_move(head_pos, tail_pos):
                     if should_tail_move_straight(head_pos, tail_pos):
                         tail_pos[1] -= 1
+                        positions_visited.add((tail_pos[0], tail_pos[1]))
                     else:  # diag
                         tail_pos[0] = head_pos[0]
                         tail_pos[1] -= 1
                         positions_visited.add((tail_pos[0], tail_pos[1]))
         elif direction == "L":
-            for x in range(distance + 1):
+            for x in range(distance):
                 head_pos[0] -= 1
                 if should_tail_move(head_pos, tail_pos):
                     if should_tail_move_straight(head_pos, tail_pos):
                         tail_pos[0] -= 1
+                        positions_visited.add((tail_pos[0], tail_pos[1]))
                     else:  # diag
                         tail_pos[1] = head_pos[1]
                         tail_pos[0] -= 1
                         positions_visited.add((tail_pos[0], tail_pos[1]))
         elif direction == "R":
-            for x in range(distance + 1):
+            for x in range(distance):
                 head_pos[0] += 1
                 if should_tail_move(head_pos, tail_pos):
                     if should_tail_move_straight(head_pos, tail_pos):
                         tail_pos[0] += 1
+                        positions_visited.add((tail_pos[0], tail_pos[1]))
                     else:  # diag
                         tail_pos[1] = head_pos[1]
                         tail_pos[0] += 1
                         positions_visited.add((tail_pos[0], tail_pos[1]))
+        # debug
+        print("end of moves")
+        print(f"{direction} {distance}")
+        print(f"{head_pos=}")
+        print(f"{tail_pos=}")
+        print("**************")
+                # end debug
     return len(positions_visited)
 
 
@@ -74,7 +89,7 @@ def should_tail_move(head_position: list, tail_position: list) -> bool:
 
     If they are diagonal, they are touching.
     """
-    if abs(head_position[0] - tail_position[0]) <= 1 or abs(head_position[1] - tail_position[1]) <= 1:  # same row/col
+    if abs(head_position[0] - tail_position[0]) <= 1 and abs(head_position[1] - tail_position[1]) <= 1:  # same row/col
         return False
     # handle diagonals - top left
     if head_position[0] == (tail_position[0] - 1) and head_position[1] == (tail_position[1] + 1):
@@ -101,6 +116,6 @@ def should_tail_move_straight(head_position: list, tail_position: list) -> bool:
 
 
 if __name__ == "__main__":
-    move_list = input_per_line("../input.txt")
+    move_list = input_per_line("../sample_input.txt")
     positions_visited_part_1 = simulation(move_list)
     print(f"The tail visited {positions_visited_part_1} unique locations.")
