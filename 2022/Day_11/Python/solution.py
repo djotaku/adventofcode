41751215338a1ed9f25303d3a0ba9837ff705de0
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-11 12:08:55 -0500

    2022 Day 11 Python Part 1
    Current solutoin gets slower and slower and locks up my computer

---
+++ 2022/Day_11/Python/solution.py
@@ -0,0 +1,72 @@
+"""Solution for AoC 2022 Day 11 - Monkey in the Middle"""
+import re
+from collections import deque
+
+def parse_monkeys(input_file: str) -> dict:
+    """Parse the input and put into a dictionary."""
+    with open(input_file) as file:
+        monkey_dict = {}
+        this_monkey_number = -9999  # fake value
+        for line in file.readlines():
+            if line.startswith("Monkey"):
+                regex = re.compile(r'(\d+)')
+                this_monkey_number = int(re.findall(regex, line)[0])
+                monkey_dict[this_monkey_number] = {}
+            elif line.__contains__("Starting items"):
+                regex = re.compile(r'(\d+)')
+                starting_items_text = re.findall(regex, line)
+                starting_items = deque()
+                for item in starting_items_text:
+                    starting_items.append(int(item))
+                monkey_dict[this_monkey_number]["starting_items"] = starting_items
+            elif line.__contains__("Operation"):
+                operation = line.split("=")
+                monkey_dict[this_monkey_number]["operation"] = operation[1].lstrip().rstrip()
+            elif line.__contains__("Test"):
+                regex = re.compile(r'(\d+)')
+                test_value = int(re.findall(regex, line)[0])
+                monkey_dict[this_monkey_number]["test"] = test_value
+            elif line.__contains__("true"):
+                regex = re.compile(r'(\d+)')
+                true_monkey = int(re.findall(regex, line)[0])
+                monkey_dict[this_monkey_number]["true_monkey"] = true_monkey
+            elif line.__contains__("false"):
+                regex = re.compile(r'(\d+)')
+                false_monkey = int(re.findall(regex, line)[0])
+                monkey_dict[this_monkey_number]["false_monkey"] = false_monkey
+            monkey_dict[this_monkey_number]["inspected"] = 0
+    return monkey_dict
+
+def monkey_in_the_middle(monkeys: dict, rounds: int) -> dict:
+    """Do the monkey rounds and return the monkey dict with the inspected numbers updated."""
+    for this_round in range(rounds+1):
+        print(f"We are in round: {this_round}.")
+        for monkey in monkeys.keys():
+            for item in monkeys[monkey]["starting_items"]:
+                monkeys[monkey]["inspected"] += 1
+                operations = monkeys[monkey]["operation"].split()
+                value_for_next_monkey = 0
+                if operations[1] == "+":
+                    value_for_next_monkey = item + int(operations[2])
+                elif operations[1] == "*":
+                    if operations[2] == "old":
+                        value_for_next_monkey = item * item
+                    else:
+                        value_for_next_monkey = item * int(operations[2])
+                value_for_next_monkey = value_for_next_monkey / 3
+                if value_for_next_monkey % monkeys[monkey]["test"] == 0:
+                    monkey_to_pass_true = monkeys[monkey]["true_monkey"]
+                    monkeys[monkey_to_pass_true]["starting_items"].append(value_for_next_monkey)
+                else:
+                    monkey_to_pass_false = monkeys[monkey]["false_monkey"]
+                    monkeys[monkey_to_pass_false]["starting_items"].append(value_for_next_monkey)
+    return monkeys
+
+if __name__ == "__main__":
+    our_input = parse_monkeys("../sample_input.txt")
+    print(our_input)
+    game_over = monkey_in_the_middle(our_input, 20)
+    print(game_over)
+    # find active monkeys
+    for monkey in game_over.keys():
+        
--- 2022/Day_11/README.md
+++ 2022/Day_11/README.md
@@ -1,17 +1,17 @@
-
-    
-# Title of Problem
+# Day 11 - Monkey in the Middle
 
 Out of respect for the author's wishes, I only put as much of the problem text as necessary to understand the problem.
 
 Visit the version with all the story elements [here](https://adventofcode.com/2022/day/11).
 
 ## Part 1
-
+Answer: 
 ## Part 2
-
+Answer: 
 ## Commentary / Approach to the Problem
-###Part 1
+### Part 1
+Because we have to keep track of how many times a monkey has inspected an item, I will have a dictionary of monkeys. One key will be items and it will hold a deque of items so I can do a pop from the left. Another key will hold a count of inspected items. 
+Other keys will hold their operation and test values.
 
 ### Part 2
 ## What I Learned
