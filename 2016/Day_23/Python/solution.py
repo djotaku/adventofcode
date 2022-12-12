175fc64a607b43c5690f22ff3ac40cbabcd7cb3a
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-11 20:10:25 -0500

    2016 Day 23 Part 1 Completed

--- 2016/Day_23/Python/solution.py
+++ 2016/Day_23/Python/solution.py
@@ -17,43 +17,81 @@ def run_bunny_assembly(registers: dict) -> dict:
     """
     counter = 0
     while counter < len(our_input):
+        print(f"{our_input=}")
         components = our_input[counter].split()
         instruction = components[0]
+        print(components)
         x = components[1]
         y = 0
         if len(components) == 3:
             y = components[2]
         match instruction:
-            case "cpy":
+            case "cpy":  # copies x (either an integer or the value of a register) into register y
+                if isinstance(y, int):
+                    break
                 if x == "a" or x == "b" or x == "c" or x == "d":
                     registers[y] = int(registers[x])
                 else:
                     registers[y] = int(x)
                 counter += 1
-            case "inc":
+            case "inc":  # increases the value of register x by one
                 registers[x] += 1
                 counter += 1
-            case "dec":
+            case "dec":  # decreases the value of register x by one
                 registers[x] -= 1
                 counter += 1
-            case "jnz":
+            case "jnz":  # jumps to an instruction y away (positive means forward; negative means backward), but only if x is not zero.
+                jump_location = 0
+                if y in ["a", "b", "c", "d"]:
+                    jump_location = registers[y]
+                else:
+                    jump_location = int(y)
                 if x in ["a", "b", "c", "d"]:
                     if registers[x] != 0:
-                        counter += int(y)
+                        counter += jump_location
                     else:
                         counter += 1
                 else:
                     if x != 0:
-                        counter += int(y)
+                        counter += jump_location
                     else:
                         counter += 1
             # need to add in the tgl instruction
+            # need a try/catch in case it tries to modify a spot outside the list
+            case "tgl":
+                distance = 0
+                if x in ["a", "b", "c", "d"]:
+                    distance = registers[x]
+                else:
+                    distance = int(x)
+                try:
+                    modified_command = toggle_transform(our_input[counter + distance])
+                    our_input[counter + distance] = modified_command
+                except:
+                    print("tried to go beyond the list")
+                counter += 1
+        print(registers)
     return registers
 
 
+def toggle_transform(assembly: str) -> str:
+    """Take in assembly code and transform it based on toggle rules. Return the new string."""
+    split = assembly.split()
+    if len(split) == 2:
+        if split[0] == "inc":
+            split[0] = "dec"
+        else:
+            split[0] = "inc"
+    elif len(split) == 3:
+        if split[0] == "jnz":
+            split[0] = "cpy"
+        else:
+            split[0] = "jnz"
+    return " ".join(split)
+
 if __name__ == "__main__":
     our_input = input_per_line('../input.txt')
-    part_1_initial_registers = {"a": 0, "b": 0, "c": 0, "d": 0}
+    part_1_initial_registers = {"a": 7, "b": 0, "c": 0, "d": 0}
     part_1_final_registers = run_bunny_assembly(part_1_initial_registers)
     print("Part 1:")
     print(f"{part_1_final_registers=}")
--- 2016/Day_23/README.md
+++ 2016/Day_23/README.md
@@ -5,19 +5,22 @@ Out of respect for the author's wishes, I only put as much of the problem text a
 Visit the version with all the story elements [here](https://adventofcode.com/2016/day/23).
 
 ## Part 1
-
+Answer: 11500
 ## Part 2
 
 ## Commentary / Approach to the Problem
 ### Part 1
 Initial idea is to create a function that will figure out what to change with tgl. Might do if/else or match statements.
 Need to check not only the length of the list (how many arguments), but also if the toggled instruction is invalid.
+
+### Part 2
+I am guessing I need to change something to multiplication or the program runs forever? He mentions "add one", so I figure I can change that to multiply?
 ## What I Learned
 
 ### Generic
 
 ### Python
-
+- Nothing
 ### Ruby
 
 ### Perl
