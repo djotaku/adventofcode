b287efde9324e9df1ee4e1ccdb5fc9438b796935
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-11 21:30:13 -0500

    2016 Day 25 Python Part 1
    - Testing, but not yet finding the right answer

--- 2016/Day_25/Python/solution.py
+++ 2016/Day_25/Python/solution.py
@@ -2,7 +2,6 @@
 import math
 
 
-
 def input_per_line(file: str):
     """This is for when each line is an input to the puzzle. The newline character is stripped."""
     with open(file, 'r') as input_file:
@@ -16,6 +15,7 @@ def run_bunny_assembly(registers: dict, part_two=False) -> dict:
     Returns registers at the end.
     """
     counter = 0
+    output = []
     while counter < len(our_input):
         # print(f"{our_input=}")
         components = our_input[counter].split()
@@ -41,7 +41,6 @@ def run_bunny_assembly(registers: dict, part_two=False) -> dict:
                 registers[x] -= 1
                 counter += 1
             case "jnz":  # jumps to an instruction y away (positive means forward; negative means backward), but only if x is not zero.
-                jump_location = 0
                 if y in ["a", "b", "c", "d"]:
                     jump_location = registers[y]
                 else:
@@ -52,7 +51,7 @@ def run_bunny_assembly(registers: dict, part_two=False) -> dict:
                     else:
                         counter += 1
                 else:
-                    if x != 0:
+                    if int(x) != 0:
                         counter += jump_location
                     else:
                         counter += 1
@@ -70,8 +69,14 @@ def run_bunny_assembly(registers: dict, part_two=False) -> dict:
                 except:
                     print("tried to go beyond the list")
                 counter += 1
-        # print(registers)
-    return registers
+            case "out":
+                if x in ["a", "b", "c", "d"]:
+                    output.append(registers[x])
+                else:
+                    output.append(x)
+        if len(output) == 20:
+            return output
+    return output
 
 
 def toggle_transform(assembly: str) -> str:
@@ -89,7 +94,16 @@ def toggle_transform(assembly: str) -> str:
             split[0] = "jnz"
     return " ".join(split)
 
+
 if __name__ == "__main__":
     our_input = input_per_line('../input.txt')
-    part_1_initial_registers = {"a": 7, "b": 0, "c": 0, "d": 0}
-    part_1_final_registers = run_bunny_assembly(part_1_initial_registers)
+    output_signals = []
+    for a in range(100000):
+        print(f"we're trying {a=}")
+        part_1_initial_registers = {"a": a, "b": 0, "c": 0, "d": 0}
+        output_signals = run_bunny_assembly(part_1_initial_registers)
+        print(f"testing output: {output_signals}")
+        if output_signals[0] == 0 and output_signals[1] == 1 and output_signals[2] == 0 and output_signals[3] == 1:
+            print(output_signals)
+            print(f"{a}")
+            break
