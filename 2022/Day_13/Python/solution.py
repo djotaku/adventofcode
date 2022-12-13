47519fe2505d9ffe67d9a92895394224808be524
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-13 07:25:05 -0500

    2022 Day 13 Python Part 1
    adding in solution.py

---
+++ 2022/Day_13/Python/solution.py
@@ -0,0 +1,6 @@
+"""Solution to AoC 2022 Day 13 - Distress Signal"""
+
+# for parsing input, have it put the pairs into pair tuple or lists so you don't have to do that later in the program
+
+if __name__ == "__main__":
+    print("We've received a distress signal!")
