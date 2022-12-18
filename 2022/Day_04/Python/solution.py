a534a6928125fd00e5110936976cf583930946e1
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-18 16:21:46 -0500

    2022 Day 4 Python Sourcery Optimizations

--- 2022/Day_04/Python/solution.py
+++ 2022/Day_04/Python/solution.py
@@ -1,4 +1,6 @@
 """Solution to AoC Day 04 - Camp Cleanup"""
+
+
 def input_per_line(file: str):
     """This is for when each line is an input to the puzzle. The newline character is stripped."""
     with open(file, 'r') as input_file:
@@ -7,35 +9,23 @@ def input_per_line(file: str):
 
 def contained_pairs(pair: str) -> int:
     """Figure out if one pair fully contains the other."""
-    elf_one = set()
-    elf_two = set()
     elf_one_list, elf_two_list = pair.split(",")
     elf_one_start, elf_one_end = elf_one_list.split("-")
     elf_two_start, elf_two_end = elf_two_list.split("-")
-    for number in range(int(elf_one_start), int(elf_one_end)+1):
-        elf_one.add(number)
-    for number in range(int(elf_two_start), int(elf_two_end)+1):
-        elf_two.add(number)
-    if elf_one.issubset(elf_two) or elf_two.issubset(elf_one):
-        return 1
-    else: return 0
+    elf_one = set(range(int(elf_one_start), int(elf_one_end)+1))
+    elf_two = set(range(int(elf_two_start), int(elf_two_end)+1))
+    return 1 if elf_one.issubset(elf_two) or elf_two.issubset(elf_one) else 0
 
 
 def check_partial_overlap(pair: str) -> int:
     """Use isdisjoint and return 1 if it's false."""
-    elf_one = set()
-    elf_two = set()
     elf_one_list, elf_two_list = pair.split(",")
     elf_one_start, elf_one_end = elf_one_list.split("-")
     elf_two_start, elf_two_end = elf_two_list.split("-")
-    for number in range(int(elf_one_start), int(elf_one_end) + 1):
-        elf_one.add(number)
-    for number in range(int(elf_two_start), int(elf_two_end) + 1):
-        elf_two.add(number)
-    if elf_one.isdisjoint(elf_two):
-        return 0
-    else:
-        return 1
+    elf_one = set(range(int(elf_one_start), int(elf_one_end) + 1))
+    elf_two = set(range(int(elf_two_start), int(elf_two_end) + 1))
+    return 0 if elf_one.isdisjoint(elf_two) else 1
+
 
 if __name__ == "__main__":
     cleanup_sections = input_per_line("../input.txt")
