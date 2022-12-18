3020d9bc833933b65ee282f95628045589994c59
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-18 09:28:28 -0500

    2022 Day 18 Part 1 Python
    Well, FINALLY, an easy solution (at least for the sample input)

---
+++ 2022/Day_18/Python/solution.py
@@ -0,0 +1,34 @@
+"""Solution to AoC 2022 Day 18 - Boiling Boulders."""
+
+
+def input_per_line(file: str):
+    """This is for when each line is an input to the puzzle. The newline character is stripped."""
+    with open(file, 'r') as input_file:
+        return [line.rstrip().split(",") for line in input_file.readlines()]
+
+
+if __name__ == "__main__":
+    debug = True
+    our_input_file = "../sample_input.txt" if debug else "../input.txt"
+    boulder_list = input_per_line(our_input_file)
+    highest_number_of_sides = len(boulder_list) * 6
+    connected_sides_count = 0
+    sides = set()
+    for boulder in boulder_list:
+        xy = f"(x={boulder[0]},y={boulder[1]})"
+        if xy in sides:
+            connected_sides_count += 1
+        else:
+            sides.add(xy)
+        yz = f"(y={boulder[1]},z={boulder[2]})"
+        if yz in sides:
+            connected_sides_count += 1
+        else:
+            sides.add(yz)
+        zx = f"(z={boulder[2]},x={boulder[0]})"
+        if zx in sides:
+            connected_sides_count += 1
+        else:
+            sides.add(zx)
+    unconnected_sides = highest_number_of_sides - connected_sides_count
+    print(f"There are {unconnected_sides} unconnected sides.")
--- 2022/Day_18/README.md
+++ 2022/Day_18/README.md
@@ -1,6 +1,4 @@
-
-    
-# Title of Problem
+# Day 18 - Boiling Boulders
 
 Out of respect for the author's wishes, I only put as much of the problem text as necessary to understand the problem.
 
@@ -11,8 +9,10 @@ Visit the version with all the story elements [here](https://adventofcode.com/20
 ## Part 2
 
 ## Commentary / Approach to the Problem
-###Part 1
-
+### Part 1
+First attempt at figuring out an answer:
+- Number of lines in input * 6 is the max number of sides
+- I think each time a pair of points match, that's a side that matches so tally those up and and subtract from the above number. (Of course, the logic is in the tally)
 ### Part 2
 ## What I Learned
 
