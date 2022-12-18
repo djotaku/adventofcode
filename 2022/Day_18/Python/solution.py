8cbd0c815263e181f26856632cb23ab007137d5c
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-18 15:49:23 -0500

    2022 Day 18 Part 1 Python
    New approach works for both samples, but not the real input. Progress?

--- 2022/Day_18/Python/solution.py
+++ 2022/Day_18/Python/solution.py
@@ -1,5 +1,5 @@
 """Solution to AoC 2022 Day 18 - Boiling Boulders."""
-
+from collections import defaultdict
 
 def input_per_line(file: str):
     """This is for when each line is an input to the puzzle. The newline character is stripped."""
@@ -15,25 +15,35 @@ if __name__ == "__main__":
     highest_number_of_sides = len(boulder_list) * 6
     print(f"{highest_number_of_sides=}")
     connected_sides_count = 0
-    sides = set()
+    cube_locations = defaultdict(bool)
     for boulder in boulder_list:
-        xy = f"(x={boulder[0]},y={boulder[1]})"
-        if xy in sides:
-            connected_sides_count += 2
-        else:
-            sides.add(xy)
-        yz = f"(y={boulder[1]},z={boulder[2]})"
-        if yz in sides:
-            connected_sides_count += 2
-        else:
-            sides.add(yz)
-        zx = f"(z={boulder[2]},x={boulder[0]})"
-        if zx in sides:
-            connected_sides_count += 2
-        else:
-            sides.add(zx)
-    unconnected_sides = highest_number_of_sides - connected_sides_count
+        cube_locations[(int(boulder[0]), int(boulder[1]), int(boulder[2]))] = True
+    checked_points = set()
+    for index in range(len(boulder_list)):  # minus 1 to not count the last one
+        # check for x neighbors
+        if cube_locations[(int(boulder_list[index][0]) + 1), int(boulder_list[index][1]), int(boulder_list[index][2])] is True:
+            this_point = f"({int(boulder_list[index][0]) + 1}, {int(boulder_list[index][1])}, {int(boulder_list[index][2])})"
+            checked_points.add(this_point)
+        if cube_locations[(int(boulder_list[index][0]) - 1), int(boulder_list[index][1]), int(boulder_list[index][2])] is True:
+            this_point = f"({int(boulder_list[index][0]) - 1}, {int(boulder_list[index][1])}, {int(boulder_list[index][2])})"
+            checked_points.add(this_point)
+        # check for y neighbors
+        if cube_locations[(int(boulder_list[index][0])), (int(boulder_list[index][1]) + 1), int(boulder_list[index][2])] is True:
+            this_point = f"({int(boulder_list[index][0])}, {int(boulder_list[index][1]) + 1}, {int(boulder_list[index][2])})"
+            checked_points.add(this_point)
+        if cube_locations[(int(boulder_list[index][0])), (int(boulder_list[index][1]) - 1), int(boulder_list[index][2])] is True:
+            this_point = f"({int(boulder_list[index][0])}, {int(boulder_list[index][1]) -1}, {int(boulder_list[index][2])})"
+            checked_points.add(this_point)
+        # check for z neighbors
+        if cube_locations[(int(boulder_list[index][0])), int(boulder_list[index][1]), (int(boulder_list[index][2])+1)] is True:
+            this_point = f"({int(boulder_list[index][0])}, {int(boulder_list[index][1])}, {int(boulder_list[index][2])+1})"
+            checked_points.add(this_point)
+        if cube_locations[(int(boulder_list[index][0])), int(boulder_list[index][1]), (int(boulder_list[index][2])-1)] is True:
+            this_point = f"({int(boulder_list[index][0])}, {int(boulder_list[index][1])}, {int(boulder_list[index][2])-1})"
+            checked_points.add(this_point)
+    unconnected_sides = (highest_number_of_sides + 2) - (len(checked_points)*2)
     print(f"There are {unconnected_sides} unconnected sides.")
 
 
 # 9298 is too high
+# 11094 is too high
--- 2022/Day_18/README.md
+++ 2022/Day_18/README.md
@@ -13,6 +13,16 @@ Visit the version with all the story elements [here](https://adventofcode.com/20
 First attempt at figuring out an answer:
 - Number of lines in input * 6 is the max number of sides
 - I think each time a pair of points match, that's a side that matches so tally those up and and subtract from the above number. (Of course, the logic is in the tally)
+
+That approach didn't really take into account the 3D space.
+
+Second attempt: 
+
+- Group cubes by Z-plane
+- Test if cubes are touching on X-axis and remove 2 sides if they are
+- Test if cubes are touching on Y-axis and remove 2 sides if they are
+
+or simplify things? Put all cubes into a dict. Key is the coordinate. Test each cube to see if it's a neighbor with others. Do not test last cube or you'll get double-counts.
 ### Part 2
 ## What I Learned
 
@@ -31,4 +41,3 @@ First attempt at figuring out an answer:
 ### Rust
 
 ### Julia
-    
