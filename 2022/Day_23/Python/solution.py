3158196ce785aade5db139dbde670114055112ed
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-23 20:38:44 -0500

    2022 Day 23 Part 1 Python
    Need to change this because checking around the map changes the dict's size

--- 2022/Day_23/Python/solution.py
+++ 2022/Day_23/Python/solution.py
@@ -1,15 +1,16 @@
 """Solution to AoC 2022 Day 23 - Unstable Diffusion."""
 
-
 import itertools
 from collections import defaultdict, deque
 
 MOVES = deque(["N", "S", "W", "E"])
 
+
 def input_per_line(file: str):
     """This is for when each line is an input to the puzzle. The newline character is stripped."""
     with open(file, 'r') as input_file:
         return [line.rstrip() for line in input_file.readlines()]
+
 
 def create_map(elves_in_field: list) -> dict:
     """Take in a list with elf positions and return a dictionary.
@@ -33,7 +34,7 @@ def elves_move(elf_map: dict) -> dict:
     # NW, N, NE, E, SE, S, SW, W, NW
     row_check = [-1, -1, -1, 0, 1, 1, 1, 0, -1]
     column_check = [-1, 0, 1, 1, 1, 0, -1, -1, -1]
-    proposed_moves = {}
+    proposed_moves = defaultdict(list)
     for location, elf in elf_map.items():
         if elf:  # we have an elf at this location
             elf_neighbors = [elf_map[((location[0] + x), (location[1] + y))] for x, y in itertools.product(column_check,
@@ -45,9 +46,35 @@ def elves_move(elf_map: dict) -> dict:
                 for move in MOVES:
                     match move:
                         case "N":
-                            pass
-            # break if they can go to a particular direction
-            # after doing this for each of the directions - do the deque pop/push
+                            northern_neighbors = [elf_map[((location[0] + x), (location[1] - 1))] for x in [-1, 0, 1]]
+                            if not any(northern_neighbors):
+                                proposed_moves[((location[0]), (location[1] - 1))].append((location[0], location[1]))
+                                break
+                        case "S":
+                            southern_neighbors = [elf_map[((location[0] + x), (location[1] + 1))] for x in [-1, 0, 1]]
+                            if not any(southern_neighbors):
+                                proposed_moves[((location[0]), (location[1] + 1))].append((location[0], location[1]))
+                                break
+                        case "E":
+                            eastern_neighbors = [elf_map[((location[0] + 1), (location[1] + y))] for y in [-1, 0, 1]]
+                            if not any(eastern_neighbors):
+                                proposed_moves[((location[0] + 1), (location[1]))].append((location[0], location[1]))
+                                break
+                        case "W":
+                            western_neighbors = [elf_map[((location[0] - 1), (location[1] + y))] for y in [-1, 0, 1]]
+                            if not any(western_neighbors):
+                                proposed_moves[((location[0] - 1), (location[1]))].append((location[0], location[1]))
+                                break
+    # see who gets to move now
+    for proposed_location, elf_list in proposed_moves.items():
+        if len(elf_list) == 1:
+            # only 1 elf wanted to move there. The elf can move.
+            elf = elf_list[0]
+            elf_map[(elf[0], elf[1])] = False
+            elf_map[(proposed_location[0], proposed_location[1])] = True
+    rotating_move = MOVES.popleft()
+    MOVES.append(rotating_move)
+    return elf_map
 
 
 def find_bounding_rectangle(elf_map: dict) -> list:
@@ -55,5 +82,17 @@ def find_bounding_rectangle(elf_map: dict) -> list:
     pass
 
 
-
-
+if __name__ == "__main__":
+    debug = "small"
+    match debug:
+        case "small":
+            our_file = "../tiny_sample_input.txt"
+        case "large":
+            our_file = "../large_sample_input.txt"
+        case "input":
+            our_file = "../input.txt"
+    elf_positions = input_per_line(our_file)
+    mapped_elves = create_map(elf_positions)
+    for _ in range(10):
+        mapped_elves = elves_move(mapped_elves)
+    # next need to do the rectangle and count empty spots
