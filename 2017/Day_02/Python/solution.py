8192bb2e0956d717b58174c9666d57167392f3f9
Eric Mesa <ericsbinaryworld@gmail.com>
2022-12-27 15:27:02 -0500

    2017 Day 02 Python completed

--- 2017/Day_02/Python/solution.py
+++ 2017/Day_02/Python/solution.py
@@ -12,7 +12,26 @@ def row_difference(row: str) -> int:
     nums = [int(number) for number in nums]
     return max(nums) - min(nums)
 
+
+def find_even_division(row:str) -> int:
+    """Take in a row and find the two numbers that evenly divide. Return the result of that division."""
+    nums = row.split()
+    nums = [int(number) for number in nums]
+    row_length = len(nums)
+    for num in nums:
+        for index in range(row_length):
+            if num % nums[index] == 0 and num != nums[index]:
+                return num // nums[index]
+
 if __name__ == "__main__":
-    spreadsheet = input_per_line("../input.txt")
+    debug = False
+    if debug:
+        input_file = "../sample_input.txt"
+    else:
+        input_file = "../input.txt"
+    spreadsheet = input_per_line(input_file)
     rows = [row_difference(row) for row in spreadsheet]
     print(f"The checksum is {sum(rows)}.")
+    row_modulo = [find_even_division(row) for row in spreadsheet]
+    print(row_modulo)
+    print(f"The checksum is actually {sum(row_modulo)}.")
