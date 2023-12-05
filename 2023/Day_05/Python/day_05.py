edc15ac8bb777f4824c49eabd385cddc119a6cac
Eric Mesa <ericsbinaryworld@gmail.com>
2023-12-05 18:41:44 -0500

    Day 05 Python Part 1
    Works for sample, kills computer on real data

--- 2023/Day_05/Python/day_05.py
+++ 2023/Day_05/Python/day_05.py
@@ -1,4 +1,5 @@
 """Solution to AoC 2023 Day 5: If You Give A Seed A Fertilizer"""
+import re
 
 map_of_maps = {}
 
@@ -52,8 +53,54 @@ def fill_in_map_of_maps(mappings: list[str]):
             map_of_maps[the_key] = generate_map(the_numbers.copy())
             the_key = ""
             the_numbers.clear()
+    # final entry
+    map_of_maps[the_key] = generate_map(the_numbers)
+
+
+def get_seeds(seed_line: str) -> list[int]:
+    """Given the seed line, extract the seeds."""
+    regex = re.compile(r'(\d+)')
+    numbers = re.findall(regex, seed_line)
+    return [int(number) for number in numbers]
+
+
+def find_location(seed: int) -> int:
+    soil_location = map_of_maps["seed-to-soil"].get(seed)
+    if soil_location is None:
+        soil_location = seed
+    # print(f"{soil_location=}")
+    fertilizer_location = map_of_maps["soil-to-fertilizer"].get(soil_location)
+    if fertilizer_location is None:
+        fertilizer_location = soil_location
+    # print(f"{fertilizer_location=}")
+    water_location = map_of_maps["fertilizer-to-water"].get(fertilizer_location)
+    if water_location is None:
+        water_location = fertilizer_location
+    # print(f"{water_location=}")
+    light_location = map_of_maps["water-to-light"].get(water_location)
+    if light_location is None:
+        light_location = water_location
+    # print(f"{light_location=}")
+    temperature_location = map_of_maps["light-to-temperature"].get(light_location)
+    if temperature_location is None:
+        temperature_location = light_location
+    # print(f"{temperature_location=}")
+    humidity_location = map_of_maps["temperature-to-humidity"].get(temperature_location)
+    if humidity_location is None:
+        humidity_location = temperature_location
+    location_location = map_of_maps["humidity-to-location"].get(humidity_location)
+    if location_location is None:
+        location_location = humidity_location
+    return location_location
 
 
 if __name__ == '__main__':
-    seeds, mappings = input_per_line_unique_first_line("../sample_input.txt")
+    seed_row, mappings = input_per_line_unique_first_line("../sample_input.txt")
+    print("filling in map")
     fill_in_map_of_maps(mappings)
+    print(mappings)
+    # print(map_of_maps["humidity-to-location"])
+    print("getting seeds")
+    seeds = get_seeds(seed_row)
+    seed_locations = [find_location(seed) for seed in seeds]
+    print(f"The lowest location is {min(seed_locations)}")
--- 2023/Day_05/Python/test_day_05.py
+++ 2023/Day_05/Python/test_day_05.py
@@ -6,3 +6,9 @@ def test_generate_map():
     test_map = day_05.generate_map(seed_to_soil)
     assert test_map[98] == 50
     assert test_map[99] == 51
+
+
+def test_get_seeds():
+    seed_line = "seeds: 79 14 55 13"
+    assert day_05.get_seeds(seed_line) == [79, 14, 55, 13]
+
